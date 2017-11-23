21c894fe4fdae68a4b588941595a58dbdc81b08c
Paul Heistracher <e1225725@student.tuwien.ac.at>
2017-11-23 16:17:00 +0100

    starting cleanup

--- interface/interface.pyx
+++ interface/interface.pyx
@@ -25,7 +25,6 @@ cdef extern from "../src/mesh.hpp":
     double dx,dy,dz;
     int n0_exp, n1_exp, n2_exp;
     Mesh (int, int, int, double, double, double)
-    void printme()
 
 cdef class pyMesh:
   cdef Mesh* thisptr
@@ -33,87 +32,8 @@ cdef class pyMesh:
     self.thisptr = new Mesh(a,b,c,d,e,f)
   #def __dealloc__(self):
   #  del self.thisptr
-
   def n0(self):
     return self.thisptr.n0
-
-cdef extern from "../src/param.hpp":
-  cdef cppclass Param:
-    double mu0;
-    double gamma;
-    double ms,A,alpha;
-    double p;
-#    bool afsync;
-    int mode;
-    double D;
-    double Ku1;
-    double D_axis[3];
-    double Ku1_axis[3];
-    double J_atom;
-    double D_atom;
-    double D_atom_axis[3];
-    double K_atom;
-    double K_atom_axis[3];
-
-cdef class pyParam:
-  cdef Param* thisptr
-  def __cinit__(self):
-    self.thisptr = new Param ()  
-  #def __dealloc__(self):
-  #  del self.thisptr
-
-  def gamma(self,value):
-    self.thisptr.gamma=value
-  def ms(self,value):
-    self.thisptr.ms=value
-  def alpha(self,value):
-    self.thisptr.alpha=value
-  def A(self,value):
-    self.thisptr.A=value
-  def p(self,value):
-    self.thisptr.p=value
-  def mode(self,value):
-    self.thisptr.mode=value
-  def D(self,value):
-    self.thisptr.D=value
-  def Ku1(self,value):
-    self.thisptr.Ku1=value
-  def J_atom(self,value):
-    self.thisptr.J_atom=value
-  def D_atom(self,value):
-    self.thisptr.D_atom=value
-  def K_atom(self,value):
-    self.thisptr.K_atom=value
-  #TODO
-  #uudef D_axis(self,value):
-  #  self.thisptr.D_axis[7]=value
-  def mu0(self):
-    return self.thisptr.mu0
-  def gamma(self):
-    return self.thisptr.gamma
-  def ms(self):
-    return self.thisptr.ms
-  def alpha(self):
-    return self.thisptr.alpha
-  def A(self):
-    return self.thisptr.A
-  def p(self):
-    return self.thisptr.p
-  def mode(self):
-    return self.thisptr.mode
-  def D(self):
-    return self.thisptr.D
-  def Ku1(self):
-    return self.thisptr.Ku1
-  def J_atom(self):
-    return self.thisptr.J_atom
-  def D_atom(self):
-    return self.thisptr.D_atom
-  def K_atom(self):
-    return self.thisptr.K_atom
-  
-  def print_gamma(self):
-    print self.thisptr.gamma
 
 
 cdef extern from "../src/state.hpp":
@@ -128,8 +48,8 @@ cdef extern from "../src/state.hpp":
 cdef class pyState:
   cdef State* thisptr
   def __cinit__(self, pyMesh mesh_in, pyParam param_in, m_in):
-    af.device.lock_array(m_in)#This does not avoid memory corruption caused by double free
     self.thisptr = new State (deref(mesh_in.thisptr), deref(param_in.thisptr), ctypes.addressof(m_in.arr))  
+    #af.device.lock_array(m_in)#This does not avoid memory corruption caused by double free
   #def __dealloc__(self):
   #  del self.thisptr
   def t(self):
@@ -141,7 +61,6 @@ cdef class pyState:
     m=af.Array()
     m.arr = ctypes.c_void_p(m_addr)
     #m.arr = ctypes.c_void_p(m_addr)
-
     #af.sync()
     #af.device.lock_array(m)
     #af.device.lock_array(m2)
@@ -149,22 +68,11 @@ cdef class pyState:
     ##Alternative
     #return self.thisptr.get_m_addr()
 
-    
-
-
-
-
-
-#def get_m(pystate):
-#  m_addr = pystate.pythisptr.get_m_addr()
-#  m=af.Array()
-#  m.arr = ctypes.c_void_ptr(m_addr)
-#  return m
-
 
 cdef extern from "../src/llg.hpp":
   cdef cppclass LLG:
     LLG (State state_in, vector[shared_ptr[LLGTerm]] vector_in);
+    vector[shared_ptr[LLGTerm]] Fieldterms;
     array llgstep(State& state);
     double E(const State& state);
     double cpu_time();
@@ -181,14 +89,15 @@ cdef class pyLLG:
   #  del self.thisptr
   def llgstep(self, pyState state_in):
     state_in.thisptr.m=self.thisptr.llgstep(deref(state_in.thisptr))
-    #return self.thisptr.llgstep(deref(state_in.thisptr))
   def print_E(self,pyState state_in):
     return self.thisptr.E(deref(state_in.thisptr))
   def cpu_time(self):
     return self.thisptr.cpu_time()
   def set_state0_alpha(self,value):
     self.thisptr.state0.param.alpha=value
-  #def add_terms(self,term)
+  def add_terms(self,*args):
+    for arg in args:
+      self.thisptr.Fieldterms.push_back(shared_ptr[LLGTerm] (<LLGTerm*><size_t>arg.pythisptr()))
   #  cdef vector[shared_ptr[LLGTerm]] vector_in
   #  for term in terms:
   #    vector_in.push_back(shared_ptr[LLGTerm] (<LLGTerm*><size_t>terms.pythisptr()))
@@ -255,3 +164,130 @@ cdef class pyZee:
 #TODO 
 #cdef extern from "../src/vtk_writer.hpp":
 #  cdef void af_to_vti(long int field, pyMesh mesh, std::string outputname);
+
+cdef extern from "../src/param.hpp":
+  cdef cppclass Param:
+    double mu0;
+    double gamma;
+    double ms,A,alpha;
+    double p;
+#    bool afsync;
+    int mode;
+    double D;
+    double Ku1;
+    double D_axis[3];
+    double Ku1_axis[3];
+    double J_atom;
+    double D_atom;
+    double D_atom_axis[3];
+    double K_atom;
+    double K_atom_axis[3];
+
+cdef class pyParam:
+  cdef Param* thisptr
+  def __cinit__(self):
+    self.thisptr = new Param ()  
+  #def __dealloc__(self):
+  #  del self.thisptr
+
+  def gamma(self,value):
+    self.thisptr.gamma=value
+  def ms(self,value):
+    self.thisptr.ms=value
+  def alpha(self,value):
+    self.thisptr.alpha=value
+  def A(self,value):
+    self.thisptr.A=value
+  def p(self,value):
+    self.thisptr.p=value
+  def mode(self,value):
+    self.thisptr.mode=value
+  def D(self,value):
+    self.thisptr.D=value
+  def D_axis_x(self,value):
+    self.thisptr.D_axis[0]=value
+  def D_axis_y(self,value):
+    self.thisptr.D_axis[1]=value
+  def D_axis_z(self,value):
+    self.thisptr.D_axis[2]=value
+  def D_axis(self, *args):
+    i=0
+    for arg in args:
+      self.thisptr.D_axis[i]=arg
+      i=i+1
+  def Ku1(self,value):
+    self.thisptr.Ku1=value
+  def Ku1_axis_x(self,value):
+    self.thisptr.Ku1_axis[0]=value
+  def Ku1_axis_y(self,value):
+    self.thisptr.Ku1_axis[1]=value
+  def Ku1_axis_z(self,value):
+    self.thisptr.Ku1_axis[2]=value
+  def Ku1_axis(self, *args):
+    i=0
+    for arg in args:
+      self.thisptr.Ku1_axis[i]=arg
+      i=i+1
+  def J_atom(self,value):
+    self.thisptr.J_atom=value
+  def D_atom(self,value):
+    self.thisptr.D_atom=value
+  def D_atom_axis_x(self,value):
+    self.thisptr.D_atom_axis[0]=value
+  def D_atom_axis_y(self,value):
+    self.thisptr.D_atom_axis[1]=value
+  def D_atom_axis_z(self,value):
+    self.thisptr.D_atom_axis[2]=value
+  def D_atom_axis(self, *args):
+    i=0
+    for arg in args:
+      self.thisptr.D_atom_axis[i]=arg
+      i=i+1
+  def K_atom(self,value):
+    self.thisptr.K_atom=value
+  def K_atom_axis_x(self,value):
+    self.thisptr.K_atom_axis[0]=value
+  def K_atom_axis_y(self,value):
+    self.thisptr.K_atom_axis[1]=value
+  def K_atom_axis_z(self,value):
+    self.thisptr.K_atom_axis[2]=value
+  def K_atom_axis(self, *args):
+    i=0
+    for arg in args:
+      self.thisptr.K_atom_axis[i]=arg
+      i=i+1
+
+  def print_K_atom_axis_x(self):
+    return self.thisptr.K_atom_axis[0]
+  def print_K_atom_axis_y(self):
+    return self.thisptr.K_atom_axis[1]
+  def print_K_atom_axis_z(self):
+    return self.thisptr.K_atom_axis[2]
+  #Read only:
+  def print_mu0(self):
+    return self.thisptr.mu0
+  def print_gamma(self):
+    return self.thisptr.gamma
+  def print_ms(self):
+    return self.thisptr.ms
+  def print_alpha(self):
+    return self.thisptr.alpha
+  def print_A(self):
+    return self.thisptr.A
+  def print_p(self):
+    return self.thisptr.p
+  def print_mode(self):
+    return self.thisptr.mode
+  def print_D(self):
+    return self.thisptr.D
+  def print_Ku1(self):
+    return self.thisptr.Ku1
+  def print_J_atom(self):
+    return self.thisptr.J_atom
+  def print_D_atom(self):
+    return self.thisptr.D_atom
+  def print_K_atom(self):
+    return self.thisptr.K_atom
+  def print_gamma(self):
+    print self.thisptr.gamma
+
--- interface/run.py
+++ interface/run.py
@@ -1,11 +1,11 @@
 import arrayfire as af
 import pth_mag
 import ctypes
+from copy import deepcopy
 
 af.info()
 meshvar=pth_mag.pyMesh(  100,25,1,5.e-7/100,1.25e-7/25,3.e-9)
 m=af.constant(0.0,100,25,1,3,dtype=af.Dtype.f64)
-print "n0= ", meshvar.n0()
 
 
 param=pth_mag.pyParam()
@@ -13,16 +13,15 @@ param.gamma (2.211e5)
 param.ms    (8e5)
 param.A     (1.3e-11)
 param.alpha (1)
-#TODO param.D_axis(3)
-#TODO print param.D
+#param.D_atom_axis_x(1)
+#param.K_atom_axis(1.,0.,0.)
+#param.K_atom_axis(100.,3.,30.)
+#print "paramk= ",param.print_K_atom_axis_x() , param.print_K_atom_axis_y() , param.print_K_atom_axis_z()
 
 m[1:-1,:,:,0] = af.constant(1.0,100-2,25,1,1,dtype=af.Dtype.f64);
 m[0,:,:,1]    = af.constant(1.0,1    ,25,1,1,dtype=af.Dtype.f64);
 m[-1,:,:,1]   = af.constant(1.0,1    ,25,1,1,dtype=af.Dtype.f64);
-#af.device.lock_array(m)
 pystate=pth_mag.pyState(meshvar,param,m)
-m_test=pystate.get_m()
-print "af.mean(m_test)=", af.mean(m_test)
 
 ##Alternative:
 #m_test=af.Array()
@@ -51,19 +50,20 @@ print pystate.t()
 print "switch --------------------"
 Llg.set_state0_alpha(0.02)# this should be changed in cpp version
 
-print "param =", param.mu0()
 zeeswitch = af.constant(0.0,1,1,1,3,dtype=af.Dtype.f64)
-zeeswitch[0,0,0,0]=-24.6e-3/param.mu0()
-zeeswitch[0,0,0,1]=+4.3e-3/param.mu0()
+zeeswitch[0,0,0,0]=-24.6e-3/param.print_mu0()
+zeeswitch[0,0,0,1]=+4.3e-3/param.print_mu0()
 zeeswitch[0,0,0,2]=0.0
 zeeswitch = af.tile(zeeswitch,100,25,1)
 zee=pth_mag.pyZee(zeeswitch,meshvar,param)
-Llg2=pth_mag.pyLLG(pystate,demag,exch,zee)
+Llg.add_terms(zee)
+#Llg2=pth_mag.pyLLG(pystate,demag,exch,zee)
 print pystate.t()
 #while pystate.t() < 2e-9:
-Llg2.llgstep(pystate)
+Llg.llgstep(pystate)
 print pystate.t()
-af.sync() # maybe this helps
 print "end    --------------------"
-#print "Triggering garbage collection: (waiting for core dumped)"
-#TODO print pystate.get_m()
+
+#m_test=deepcopy(pystate.get_m())
+#m_test=pystate.get_m()
+print "af.mean(m_test)=", af.mean(pystate.get_m())
