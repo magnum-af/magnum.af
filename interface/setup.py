4f688042c13d4f445dea5a800d7b0684d0742136
pth <paul.thomas.heistracher@univie.ac.at>
2017-11-27 17:32:29 +0100

    added opencl lib in setup.py

--- interface/setup.py
+++ interface/setup.py
@@ -9,7 +9,8 @@ setup(
               # Note, you can link against a c++ library
               # instead of including the source
               #libraries = ["afcuda"],
-              libraries = ["afcpu"],
+              #libraries = ["afcpu"],
+              libraries = ["afopencl"],
               #sources=["interface.pyx", "../src/mesh.cpp","../src/state.cpp","../src/micro_demag.cpp","../src/func.cpp"],
               sources=["interface.pyx", "../src/mesh.cpp","../src/state.cpp","../src/micro_demag.cpp","../src/func.cpp","../src/llg.cpp","../src/micro_exch.cpp","../src/zee.cpp"],
               extra_compile_args=['-std=gnu++14','-O3'],
