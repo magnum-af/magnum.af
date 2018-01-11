6641c3c88ea20c3f3172819ac2dea173d03590d3
pth <paul.thomas.heistracher@univie.ac.at>
2018-01-11 17:36:06 +0100

    rigid refined plotting and some cleanup

--- src/main_stoch_heun_rigid.cpp
+++ src/main_stoch_heun_rigid.cpp
@@ -1,5 +1,6 @@
 //Following paper Journal of Magnetism and Magnetic Materials 233 (2001) 296–304 from Scholz, Schrefl, Fidler
 //ATTENTION, ERROR IN PAPER EQ 22 second expression, should be: (epx(chi)-1)/(sqrt(pi*chi)*erfi(sqrt(chi)))
+//Integrals in EQ 22 only consider positive z, so we take fabs(mz) ! (this compensates switching which would lead to an average around zero)
 #include <iostream>
 #include <complex>
 #include "arrayfire.h"
@@ -7,16 +8,7 @@
 using namespace af;
 using namespace std::complex_literals;
 using Faddeeva::erfi;
- typedef std::shared_ptr<LLGTerm> llgt_ptr; 
-void calcm(State state, std::ostream& myfile);
-
-//double pth_erfi(double x){
-//    std::complex<double> cx = x ;
-//    std::complex<double> im = 0.0 + 1i;
-//    std::complex<double> result = - 1i * erf(1i*x);
-//    return result.real;
-//}
-
+typedef std::shared_ptr<LLGTerm> llgt_ptr;
 
 //Mathematica:
 //(e^x-1)/(sqrt(pi*x)*erfi(sqrt(x))) =(int(exp(x * z^2)*z) dz from 0 to 1 )/(int(exp(x * z^2)) dz from 0 to 1)
@@ -28,6 +20,9 @@ void set_m_to_z(State& state){
     state.m(span,span,span,0)=0.;
     state.m(span,span,span,1)=0.;
     state.m(span,span,span,2)=1.;
+}
+void calcm(State state, std::ostream& myfile){
+    myfile << std::setw(12) << state.t << "\t" <<meani(state.m,0)<< "\t" <<meani(state.m,1)<< "\t" <<meani(state.m,2)<< "\t" << std::endl;
 }
 
 int main(int argc, char** argv)
@@ -43,8 +38,6 @@ int main(int argc, char** argv)
     std::ofstream stream2;
     stream2.precision(12);
     stream2.open ((filepath + "rigid.dat").c_str());
-    
-
   
     setDevice(argc>2? std::stoi(argv[2]):0);
     info();
@@ -53,7 +46,9 @@ int main(int argc, char** argv)
     unsigned long int measure_steps = 6e4;
 
     stream2<<"#measure_steps = "<<measure_steps<<std::endl;
-    stream2<<"# dt << Stoch.param.T<<  mean_mz <<  abs_mean_mz << mean_mz_analytical(chi) <<Stoch.param.T<<  mean_mz << abs_mean_mz << mean_mz_analytical(chi) <<Stoch.param.T<<  mean_mz << abs_mean_mz << mean_mz_analytical(chi)<<measure_steps"<<std::endl;
+    stream2<<"# dt << Stoch.param.T<<  mean_mz <<  abs_mean_mz << mean_mz_analytical(chi) <<Stoch.param.T<<";
+    stream2<<" mean_mz << abs_mean_mz << mean_mz_analytical(chi) <<Stoch.param.T<<  mean_mz";
+    stream2<<" abs_mean_mz << mean_mz_analytical(chi)<<measure_steps"<<std::endl;
     // Parameter initialization
     const double x=1.e-9, y=1.e-9, z=1.e-9;
     const int nx = 1, ny=1 ,nz=1;
@@ -66,7 +61,6 @@ int main(int argc, char** argv)
     param.Ku1   = 6.9e6;
     param.alpha = 0.1;
     param.T = 10;
-
   
     // Initial magnetic field
     array m = constant(0.0,mesh.n0,mesh.n1,mesh.n2,3,f64);
@@ -74,7 +68,6 @@ int main(int argc, char** argv)
     State state(mesh,param, m);//ATTENTION, to be set in each loop
     std::vector<llgt_ptr> llgterm;
     Stochastic_LLG Stoch(state,llgterm,0.,"Heun");//ATTENTION, to be set in each loop
-  
   
     //Declare Variables
     double mean_mz;
@@ -99,8 +92,6 @@ int main(int argc, char** argv)
             Stoch.step(state,dt); 
             mean_mz+=afvalue(state.m(0,0,0,2));
             abs_mean_mz+=fabs(afvalue(state.m(0,0,0,2)));
-            //if(i%1000==0) std::cout<< i <<"  "<< mean_mz <<std::endl;
-            //calcm(state,stream);
         }
 
         mean_mz=mean_mz/measure_steps;
@@ -124,8 +115,6 @@ int main(int argc, char** argv)
             Stoch.step(state,dt); 
             mean_mz+=afvalue(state.m(0,0,0,2));
             abs_mean_mz+=fabs(afvalue(state.m(0,0,0,2)));
-            //if(i%1000==0) std::cout<< i <<"  "<< mean_mz <<std::endl;
-            //calcm(state,stream);
         }
 
         mean_mz=mean_mz/measure_steps;
@@ -149,8 +138,6 @@ int main(int argc, char** argv)
             Stoch.step(state,dt); 
             mean_mz+=afvalue(state.m(0,0,0,2));
             abs_mean_mz+=fabs(afvalue(state.m(0,0,0,2)));
-            //if(i%1000==0) std::cout<< i <<"  "<< mean_mz <<std::endl;
-            //calcm(state,stream);
         }
 
         mean_mz=mean_mz/measure_steps;
@@ -169,27 +156,3 @@ int main(int argc, char** argv)
     stream2.close();
     return 0;
 }
-void calcm(State state, std::ostream& myfile){
-    myfile << std::setw(12) << state.t << "\t" <<meani(state.m,0)<< "\t" <<meani(state.m,1)<< "\t" <<meani(state.m,2)<< "\t" << std::endl;
-}
-
-    //unsigned long int relax_steps   = 1e2;
-    //std::cout<<"Reference T=10        = "<< 0.98979 <<std::endl;
-    //std::cout<<"Reference T=50        = "<< 0.94268 <<std::endl;
-    //std::cout<<"Reference T=200       = "<< 0.71976 <<std::endl;
-  
-    ////RELAX
-    //timer t = af::timer::start();
-    //for (unsigned long int i = 0; i < relax_steps; i++){
-    //    Stoch.step(state,dt); 
-    //    calcm(state,stream);
-    //}
-    //double timerelax= af::timer::stop(t);
-    //std::cout<<"timerelax [af-s]: "<< timerelax <<std::endl;
-    //vti_writer_micro(state.m, mesh ,(filepath + "relax").c_str());
-
-
-    //timer t2 = af::timer::start();
-    //double timemeasure= af::timer::stop(t2);
-    //std::cout<<"timemeasure [af-s]: "<< timemeasure <<std::endl;
-
