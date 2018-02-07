f70deed5891b24b15ddcf05066391c237fd0c088
Heistracher Paul <paul.thomas.heistracher@univie.ac.at>
2018-02-07 17:29:20 +0100

    vsc3/postprocesing

--- Data/vsc3/skyrm_stoch_cpu/postprocessing/plot_m.sh
+++ Data/vsc3/skyrm_stoch_cpu/postprocessing/plot_m.sh
@@ -4,11 +4,12 @@ if [ $# -eq 0 ]
   then
     echo "Usage: ./plot.sh /path/to/inputfiles"
     echo "e.g.:  ./plot.sh \$PWD/..."
-else
-    echo "enter timestep"
-    read timestep
-    echo "enter maxID"
-    read maxID
-    gnuplot -e 'plot for [i=0:'$maxID'] "'$1'/m".i.".dat"     using ($0*'$timestep'):1 title "mz".i' -persist
-    gnuplot -e 'plot for [i=0:'$maxID'] "'$1'/m_avg".i.".dat" using ($0*'$timestep'):1 title "mz_avg".i' -persist
+    exit 1
 fi
+
+echo "enter timestep"
+read timestep
+echo "enter maxID"
+read maxID
+gnuplot -e 'plot for [i=0:'$maxID'] "'$1'/m".i.".dat"     using ($0*'$timestep'):1 title "mz".i' -persist
+gnuplot -e 'plot for [i=0:'$maxID'] "'$1'/m_avg".i.".dat" using ($0*'$timestep'):1 title "mz_avg".i' -persist
