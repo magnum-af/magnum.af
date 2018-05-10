51af2978feb76021b0b141099eccb39ea7bd31ae
paul <e1225725>
2018-05-11 01:16:02 +0200

    runall.sh adding integration sp4

--- tests/runall.sh
+++ tests/runall.sh
@@ -2,10 +2,14 @@
 
 #usage: ./runall /path/to/gitdirectory(pth-mag)
 
+#unit
 PYTHONPATH=$1/build/src/ python $1/tests/unit/atomistic_anisotropy.py
 PYTHONPATH=$1/build/src/ python $1/tests/unit/atomistic_dipole_dipole.py
 PYTHONPATH=$1/build/src/ python $1/tests/unit/atomistic_exchange.py
 PYTHONPATH=$1/build/src/ python $1/tests/unit/atomistic_dmi.py
+
+#integration
+PYTHONPATH=$1/build/src/ python $1/tests/integration/sp4.py
 
 #Planned:
 #tests for all interactions
