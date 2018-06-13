e893b7ade275385a8aec5852406e0b31ae2b7bf1
pth <paul.thomas.heistracher@univie.ac.at>
2018-06-13 15:59:59 +0200

    pgfplot.cpp add component selection

--- src/pgfplot.cpp
+++ src/pgfplot.cpp
@@ -20,6 +20,46 @@ void pgfplot_mz(af::array m, Mesh mesh, std::string outputfile, int mz_slice){
     stream.close();
 }
 
+void pgfplot_mz(af::array m, Mesh mesh, std::string outputfile, int mz_slice, int nmod){
+    std::cout<<"pgfplot_mz writing file "<< outputfile << std::endl;
+
+    std::ofstream stream;
+    stream.precision(6);
+    stream.open (outputfile.c_str());
+
+    for (int y = 0; y < m.dims(1); y++){
+        for (int x = 0; x < m.dims(0); x++){
+            if(x % nmod == nmod/2 && y % nmod == nmod/2){//NOTE: needed if file becomes too big for latex
+                stream    << 1e9*mesh.dx*x << " " << 1e9*mesh.dy*y << " " << afvalue(m(x,y,mz_slice,2))<< std::endl;
+                std::cout << 1e9*mesh.dx*x << " " << 1e9*mesh.dy*y << " " << afvalue(m(x,y,mz_slice,2))<< std::endl;
+            }
+        }
+        stream << std::endl;
+        std::cout << std::endl;
+    }
+    stream.close();
+}
+
+void pgfplot_mi(af::array m, Mesh mesh, std::string outputfile, int i, int mz_slice, int nmod){
+    std::cout<<"pgfplot_mz writing file "<< outputfile << std::endl;
+
+    std::ofstream stream;
+    stream.precision(6);
+    stream.open (outputfile.c_str());
+
+    for (int y = 0; y < m.dims(1); y++){
+        for (int x = 0; x < m.dims(0); x++){
+            if(x % nmod == nmod/2 && y % nmod == nmod/2){//NOTE: needed if file becomes too big for latex
+                stream    << 1e9*mesh.dx*x << " " << 1e9*mesh.dy*y << " " << afvalue(m(x,y,mz_slice,i))<< std::endl;
+                std::cout << 1e9*mesh.dx*x << " " << 1e9*mesh.dy*y << " " << afvalue(m(x,y,mz_slice,i))<< std::endl;
+            }
+        }
+        stream << std::endl;
+        std::cout << std::endl;
+    }
+    stream.close();
+}
+
 void pgfplot_nz_quiver(af::array m, Mesh mesh, std::string outputfile, int mz_slice, int nmod){
     std::cout<<"pgfplot_nz_quiver writing file "<< outputfile << std::endl;
     std::ofstream stream;
--- src/pgfplot.hpp
+++ src/pgfplot.hpp
@@ -7,6 +7,8 @@
 #include "func.hpp"
 
 void pgfplot_mz(af::array m, Mesh mesh, std::string outputfile, int mz_slice);
+void pgfplot_mz(af::array m, Mesh mesh, std::string outputfile, int mz_slice, int nmod);
+void pgfplot_mi(af::array m, Mesh mesh, std::string outputfile, int i, int mz_slice, int nmod);
 void pgfplot_nz_quiver(af::array m, Mesh mesh, std::string outputfile, int mz_slice, int nmod);
 
 #endif
