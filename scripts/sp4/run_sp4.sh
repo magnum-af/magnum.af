8eddad2e8c310fb16fa93c8428f1fc7391740457
Paul Heistracher <e1225725@student.tuwien.ac.at>
2018-07-05 20:31:10 +0200

    edit run_sp4.sh

--- scripts/sp4/run_sp4.sh
+++ scripts/sp4/run_sp4.sh
@@ -1,27 +1,49 @@
 #!/bin/bash
 
-if [ -e ../../src/main*.cpp ]
+magafdir=../..
+# aborting if src/main* already exists
+if [ -e $magafdir/src/main*.cpp ]
 then
     echo "To run, please remove main in magnun.af/src! Aborting..."
-    exit
+    exit 1
 else
     echo "src/ is clean, building..."
 fi
 
-cp main_sp4.cpp ../../src
-../build.sh
-rm ../../src/main_sp4.cpp
-
-if [ -e ../../bin/magnum.af-opencl ];then
-    ../../bin/magnum.af-opencl $1
-elif [ -e ../../bin/magnum.af-cuda ];then
-    ../../bin/magnum.af-cuda $1
+# determining write directory
+if [ -n "$1" ];then
+    if [ -e "$1" ];then
+        echo "writing in existing directory " $1
+    else
+        mkdir --parents $1
+        echo "writing in new directory " $1
+    fi
 else
-    ../../bin/magnum.af-cpu $1
+    if [ ! -e "$magafdir/Data/Testing" ];then
+        echo " creating default directory $magafdir/Data/Testing"
+        mkdir --parents $magafdir/Data/Testing
+    else
+        echo "writing in existing default directory $magafdir/Data/Testing"
+    fi
 fi
 
+# building
+cp main_sp4.cpp $magafdir/src
+$magafdir/scripts/build.sh
+rm $magafdir/src/main_sp4.cpp
+
+# running
+if [ -e $magafdir/bin/magnum.af-opencl ];then
+    $magafdir/bin/magnum.af-opencl $1
+elif [ -e $magafdir/bin/magnum.af-cuda ];then
+    $magafdir/bin/magnum.af-cuda $1
+else
+    $magafdir/bin/magnum.af-cpu $1
+fi
+
+# plotting
 if [ -n "$1" ]; then
     ./plot_sp4.sh $1/m.dat
 else 
-    ./plot_sp4.sh ../../Data/Testing/m.dat
+    ./plot_sp4.sh $magafdir/Data/Testing/m.dat
 fi
