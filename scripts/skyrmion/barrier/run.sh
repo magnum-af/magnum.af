e795cb4ab68d56e4dab533b3b741a4eb7db8c6cf
Paul Heistracher <e1225725@student.tuwien.ac.at>
2018-07-24 10:51:52 +0200

    skyrm/barrier/run.sh

---
+++ scripts/skyrmion/barrier/run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Usage ./run*.sh absolute/path/to/write/to main*.cpp
+
+GPU=0
+
+# calling this scripts's directory
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# relative path to magnum.af/
+magafdir=../../..
+buildfile=$2
+plotfile=plot*.sh
+
+# checking if other main exists in /src
+$magafdir/scripts/bash/check_main.sh
+
+# building 
+cp $buildfile $magafdir/src
+$magafdir/scripts/bash/build.sh $magafdir
+rm $magafdir/src/$buildfile
+
+# moving possible old main back
+$magafdir/scripts/bash/check_main_remove.sh
+
+# creating write dir
+$magafdir/scripts/bash/check_write_dir.sh $1/skyrm
+
+# copying files
+cp $magafdir/bin/magnum.af-* $1
+cp $buildfile $1
+cp $plotfile $1
+
+# running
+if [ -e $magafdir/bin/magnum.af-opencl ];then
+    screen -d -m bash -c "$magafdir/bin/magnum.af-opencl $1 $GPU > $1/cout.txt"
+else
+    $magafdir/bin/magnum.af-cpu $1 $GPU
+fi
+
+# run plot
+#./plot_skyrmion_stoch.sh $1/m0.dat
