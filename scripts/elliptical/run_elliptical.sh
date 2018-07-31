111c7aff4ef08a980a1cb67559c40ec414e374a2
paul <e1225725>
2018-07-31 12:43:53 +0200

    cuda backend in run_elliptical.sh

--- scripts/elliptical/run_elliptical.sh
+++ scripts/elliptical/run_elliptical.sh
@@ -32,7 +32,7 @@ cp $buildfile $1
 
 # running
 if [ -e $magafdir/bin/magnum.af-cuda ];then
-    screen -d -m bash -c "export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH && export PATH=/usr/local/cuda-8.0/bin:$PATH && $magafdir/bin/magnum.af-opencl $1 $2 $3> $1/cout.txt 2>&1"
+    screen -d -m bash -c "export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH && export PATH=/usr/local/cuda-8.0/bin:$PATH && $magafdir/bin/magnum.af-cuda $1 $2 $3> $1/cout.txt 2>&1"
 elif [ -e $magafdir/bin/magnum.af-opencl ];then
     screen -d -m bash -c "$magafdir/bin/magnum.af-opencl $1 $2 $3 > $1/cout.txt 2>&1"
     echo "NOTE: Opencl Backend fails due to arrayfire error 202 in fft, if chosen mesh dimensions have primefactor > 11"
