9909071d8212a40b4770934f239112189c2e5828
paul <e1225725>
2018-08-02 11:37:10 +0200

    plot_hysteresis.sh

---
+++ scripts/vortex/plot_hysteresis.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+gnuplot  -e '
+        set terminal pdf;
+	set xlabel "mu_0*H_ext [T]";
+	set ylabel "Average Magnetizaion";
+        set output "hys.pdf";
+	p 
+        "m.dat" u ($5*3.14159*4*1e-7):2 w l t "<m_x>", 
+        "m.dat" u ($5*3.14159*4*1e-7):3 w l t "<m_y>",
+        "m.dat" u ($5*3.14159*4*1e-7):4 w l t "<m_z>"'
+gnuplot  -e '
+	set xlabel "mu_0*H_ext [T]";
+	set ylabel "Average Magnetizaion";
+	p 
+        "m.dat" u ($5*3.14159*4*1e-7):2 w l t "<m_x>", 
+        "m.dat" u ($5*3.14159*4*1e-7):3 w l t "<m_y>",
+        "m.dat" u ($5*3.14159*4*1e-7):4 w l t "<m_z>"' --persist
