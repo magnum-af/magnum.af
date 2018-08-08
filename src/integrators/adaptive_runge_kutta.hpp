59f6c0f1f321d7fd3671f77d4316980722fe836a
pth <paul.thomas.heistracher@univie.ac.at>
2018-08-08 14:28:02 +0200

    new_llg devel

--- src/integrators/adaptive_runge_kutta.hpp
+++ src/integrators/adaptive_runge_kutta.hpp
@@ -5,17 +5,17 @@
 #include "../func.hpp"
 #include "../state.hpp"
 
-typedef State (*callback_function)(const State& state);
+typedef array (*callback_function)(const State& state);
 
 class AdaptiveRungeKutta {
     public:
         AdaptiveRungeKutta(callback_function f, std::string scheme = "RKF45", Controller controller = Controller());
-        void step(af::array& m, double& t);
+        void step(State&);
     private:
         callback_function f;
         const std::string scheme; //Integration scheme s.a. RKF45, DP45, ...
         Controller controller;
-        af::array RKF45(const af::array& m, const double t, const double dt, double& err);
+        af::array RKF45(State state, const double dt, double& err);
         af::array DP45(const af::array& m, const double t, const double dt, double& err);
         af::array BS45(const af::array& m, const double t, const double dt , double& err);
         double h{1.01e-15}; //step size of RK used in controller
