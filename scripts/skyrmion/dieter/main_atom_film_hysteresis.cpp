4d4709c983531df911db449168c4a1ccdf304b39
pth <paul.thomas.heistracher@univie.ac.at>
2018-10-03 18:47:03 +0200

    main_atom_film_hysteresis.cpp

--- scripts/skyrmion/dieter/main_atom_film_hysteresis.cpp
+++ scripts/skyrmion/dieter/main_atom_film_hysteresis.cpp
@@ -18,7 +18,7 @@ af::array zee_func(State state){
     double field_Tesla = 0;
     field_Tesla = rate *state.t; 
     array zee = constant(0.0,state.mesh.n0,state.mesh.n1,state.mesh.n2,3,f64);
-    zee(span,span,span,0)=constant(field_Tesla/state.param.mu0 ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
+    zee(span,span,span,2)=constant(field_Tesla/state.param.mu0 ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
     return  zee;
 }
 
@@ -94,7 +94,7 @@ int main(int argc, char** argv)
     while (state.t <  simtime){
          state.m=Llg.llgstep(state);
          if( state.steps % 1000 == 0){
-             calc_mean_m(state, stream, afvalue(Llg.Fieldterms[Llg.Fieldterms.size()-1]->h(state)(0,0,0,0)));
+             calc_mean_m(state, stream, afvalue(Llg.Fieldterms[Llg.Fieldterms.size()-1]->h(state)(0,0,0,2)));
              vti_writer_atom(state.m, mesh ,(filepath + "m_hysteresis_"+std::to_string(state.steps)).c_str());
          }
     }
