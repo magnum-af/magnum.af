5b76a92b22f2d025841531ed30933ebf652e2abe
pth <paul.thomas.heistracher@univie.ac.at>
2018-10-10 13:20:23 +0200

    dp78.cpp comment cout

--- tests/integration/cpp/dp78.cpp
+++ tests/integration/cpp/dp78.cpp
@@ -24,7 +24,7 @@ TEST(DP78IntegrationTest, n) {
     State state(Mesh(0,0,0,0,0,0), Param(), m);
     for (int i=0; i<100; i++){
          callback.step(state);
-         std::cout << "i=" << i << ", state.m= " << afvalue(state.m) << "; ";
+         //std::cout << "i=" << i << ", state.m= " << afvalue(state.m) << "; ";
          ASSERT_NEAR(afvalue(state.m), 1./16. * pow(pow(state.t,2)+4,2), 1e-8 );
     }
 }
