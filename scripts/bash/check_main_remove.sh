2397be3543ed81500e798e9e56d672e82ea0f0d1
pth <paul.thomas.heistracher@univie.ac.at>
2018-10-29 13:37:59 +0100

    bash/check_main_remove.sh

--- scripts/bash/check_main_remove.sh
+++ scripts/bash/check_main_remove.sh
@@ -7,16 +7,13 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # relative path to magnum.af/
 magafdir=../..
 
+if [ -e $magafdir/src/main*.cpp ];then
+    echo "Error in check_main_remove.sh: some main*.cpp file in magnum.af/src exists, aborting..."
+    exit 1
+fi
 
-if [ -e $magafdir/temp_main/main*.cpp ]
-then
-    if [ -e $magafdir/src/main*.cpp ]
-    then
-        echo "Error in moving temp_main to /src, main in /src still exists!"
-        exit 1
-    fi
+if [ -e $magafdir/temp_main/main*.cpp ];then
     echo "Moving temp_main to /src"
     mv $magafdir/temp_main/main*.cpp $magafdir/src
     rmdir $magafdir/temp_main
-    exit 0
 fi
