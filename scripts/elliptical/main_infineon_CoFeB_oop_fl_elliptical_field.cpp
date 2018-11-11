c0578b5dac6ddd907cc095855829fdb20b551030
Paul Heistracher <e1225725@student.tuwien.ac.at>
2018-11-11 02:20:47 +0100

    ../scripts/elliptical/main_infineon_CoFeB_oop_fl_elliptical_field.cpp

--- scripts/elliptical/main_infineon_CoFeB_oop_fl_elliptical_field.cpp
+++ scripts/elliptical/main_infineon_CoFeB_oop_fl_elliptical_field.cpp
@@ -4,40 +4,45 @@
 #include <iomanip>
 #include <fstream>
 
-double t_full_rotation = 800e-9;
-double A = 0.025/(4e-7 * M_PI); // 0.025 T is half the linear range 
-double B = 1.0 * A; // TODO
-af::array zee_func(State state){
-    double phi = 2 * M_PI * (state.t / t_full_rotation);
-    array zee = constant(0.0,state.mesh.n0,state.mesh.n1,state.mesh.n2,3,f64);
-    zee(span,span,span,0)=constant( A * std::cos(phi) ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
-    zee(span,span,span,1)=constant( B * std::sin(phi) ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
-    zee(span,span,span,2)=constant(-A * std::cos(phi) ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
-    return  zee;
-}
+using namespace af;
 
-af::array zee_func_for_relax_in_init(State state){
-    double phi = 0;
-    array zee = constant(0.0,state.mesh.n0,state.mesh.n1,state.mesh.n2,3,f64);
-    zee(span,span,span,0)=constant( A * std::cos(phi) ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
-    zee(span,span,span,1)=constant( B * std::sin(phi) ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
-    zee(span,span,span,2)=constant(-A * std::cos(phi) ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
-    return  zee;
-}
-  
+// argv[1]: filepath,  argv[2]: Device, argv[3]: A in mT, argv[4]: quotient B/A in percent, argv[5] t_full_rotation in seconds
 int main(int argc, char** argv)
 {
-    std::string filepath(argc >= 1? argv[1]: "../Data/Testing");
-    if( argc >= 1 ){ filepath.append("/");}
-    if( argc >= 2 ){ setDevice( std::stoi( argv[2]));}
-    std::string path_mrelax(argc>3? argv[3]: "");
+    std::string filepath(argc > 1? argv[1]: "../Data/Testing");
+    if( argc > 1 ){ filepath.append("/");}
+    setDevice( argc > 2 ? std::stoi( argv[2]) : 0);
+    // Input a in mT, argv[3]=25 mT is converted to 0.025 T and divided by mu0
+    const double A = double(argc > 3 ? std::stod(argv[3])*1e-3/(4e-7 * M_PI) : (double)(0.025/(4e-7 * M_PI)));
+    const double B = double(argc > 4 ? std::stod(argv[4])/100 : 1.0) * A; // Input a in percent, B=1.0 == 100%
+    const double t_full_rotation = double(argc > 5 ? std::stod(argv[5]) : (double)(800e-9));
+    const std::string path_mrelax(argc>5? argv[5]: "");
     std::cout<<"Writing into path "<<filepath.c_str()<<std::endl;
+    std::cout << "A=" << A << "B= " << B << "t_full_rotation=" << t_full_rotation << std::endl;
     std::cout.precision(24);
     info();
 
+    // Defining lamdas
+    auto zee_func_for_relax_in_init= [ A, B ] ( State state ) -> af::array {
+        double phi = 0;
+        array zee = constant(0.0,state.mesh.n0,state.mesh.n1,state.mesh.n2,3,f64);
+        zee(span,span,span,0)=constant( A * std::cos(phi) ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
+        zee(span,span,span,1)=constant( B * std::sin(phi) ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
+        zee(span,span,span,2)=constant(-A * std::cos(phi) ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
+        return  zee;
+    };
+
+    auto zee_func = [ t_full_rotation, A, B ] ( State state ) -> af::array {
+        double phi = 2 * M_PI * (state.t / t_full_rotation);
+        array zee = constant(0.0,state.mesh.n0,state.mesh.n1,state.mesh.n2,3,f64);
+        zee(span,span,span,0)=constant( A * std::cos(phi) ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
+        zee(span,span,span,1)=constant( B * std::sin(phi) ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
+        zee(span,span,span,2)=constant(-A * std::cos(phi) ,state.mesh.n0,state.mesh.n1,state.mesh.n2,1,f64);
+        return  zee;
+    };
 
     // Parameter initialization
-    const double x=800e-9, y=800e-9, z=1.3e-9;//[m] // Physical dimensions
+    const double x=800e-9, y=800e-9, z=1.0e-9;//[m] // Physical dimensions
     const int nx = 250, ny=250 ,nz=1;
     //const int nx = 400, ny=400 ,nz=1;
   
@@ -61,7 +66,7 @@ int main(int argc, char** argv)
     llgterm.push_back( LlgTerm (new DemagSolver(mesh,param)));
     llgterm.push_back( LlgTerm (new ExchSolver(mesh,param)));
     llgterm.push_back( LlgTerm (new ANISOTROPY(mesh,param)));
-    llgterm.push_back( LlgTerm (new Zee(& zee_func_for_relax_in_init)));
+    llgterm.push_back( LlgTerm (new Zee( zee_func_for_relax_in_init)));
     NewLlg Llg(llgterm);
 
     // Relaxation
@@ -85,7 +90,7 @@ int main(int argc, char** argv)
     Llg.llgterms.pop_back(); // Remove init zee field 
 
     timer t_hys = af::timer::start();
-    Llg.llgterms.push_back( LlgTerm (new Zee(&zee_func))); //Rate in T/s
+    Llg.llgterms.push_back( LlgTerm (new Zee(zee_func))); //Rate in T/s
     while (state.t < t_full_rotation){
          Llg.step(state);
          state.calc_mean_m(stream, n_cells, Llg.llgterms[Llg.llgterms.size()-1]->h(state)(0,0,0,af::span));
