6d5eabe257ff6860116677df9ece0843d689b96a
paul <paul.thomas.heistracher@univie.ac.at>
2018-11-28 12:33:56 +0100

    2to3-2.7 for scripts/sp4/sp4.py

--- scripts/sp4/sp4.py
+++ scripts/sp4/sp4.py
@@ -23,21 +23,21 @@ demag=magnum_af.pyDemagSolver(meshvar,param)
 exch=magnum_af.pyExchSolver(meshvar,param)
 Llg=magnum_af.pyLLG(demag,exch)
 
-print "relax --------------------"
-print pystate.t()
+print("relax --------------------")
+print(pystate.t())
 while pystate.t() < 1e-9:
   time1=pystate.t()
   Llg.llgstep(pystate)
   time2=pystate.t()
-  print time2-time1, time1, time2, pystate.t()
-print pystate.t()
+  print(time2-time1, time1, time2, pystate.t())
+print(pystate.t())
 #pystate.py_vti_writer_micro("/home/paul/git/pth-mag/Data/Testing/py_interf/m_relax")
 
 #teststate=magnum_af.pyState(meshvar,param,m) # testing wether teststate.m is correctly overwirtten with m_relax
 #teststate.py_vti_reader("/home/paul/git/pth-mag/Data/Testing/py_interf/m_relax.vti")
 #teststate.py_vti_writer_micro("/home/paul/git/pth-mag/Data/Testing/py_interf/m_reader")
 
-print "switch --------------------"
+print("switch --------------------")
 pystate.set_alpha(0.02)
 
 zeeswitch = af.constant(0.0,1,1,1,3,dtype=af.Dtype.f64)
@@ -47,7 +47,7 @@ zeeswitch[0,0,0,2]=0.0
 zeeswitch = af.tile(zeeswitch,100,25,1)
 zee=magnum_af.pyZee(zeeswitch)
 Llg.add_terms(zee)
-print pystate.t()
+print(pystate.t())
 intx=0
 inty=0
 intz=0
@@ -59,11 +59,11 @@ while pystate.t() < 2e-9:
   intx+=pystate.meanxyz(0)*stepsize
   inty+=pystate.meanxyz(1)*stepsize
   intz+=pystate.meanxyz(2)*stepsize
-  #print time2-time1, time1, time2, pystate.t()
-  #print pystate.meanxyz(0), pystate.meanxyz(1), pystate.meanxyz(2)
-  print intx, inty, intz
-print pystate.t()
-print "finished  --------------------"
-print "af.mean(m_test)=", af.mean(pystate.get_m())
+  print(time2-time1, time1, time2, pystate.t())
+  #print(pystate.meanxyz(0), pystate.meanxyz(1), pystate.meanxyz(2))
+  #print(intx, inty, intz)
+print(pystate.t())
+print("finished  --------------------")
+print("af.mean(m_test)=", af.mean(pystate.get_m()))
 #TODO second call causes segfault:
-print "af.mean(m_test)=", af.mean(pystate.get_m())
+print("af.mean(m_test)=", af.mean(pystate.get_m()))
