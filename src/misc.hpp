0577c45269fe0de73027dcd443590e6e4a3eea10
paul <paul.thomas.heistracher@univie.ac.at>
2018-12-04 10:54:06 +0100

    colorizing cout

--- src/misc.hpp
+++ src/misc.hpp
@@ -7,10 +7,18 @@
 unsigned long long GetDirSize(std::string filepath);
 inline bool exists (const std::string& absolute_filepath);
 
-// Returns true if file "name" exists, false otherwise
+///> Returns true if file "name" exists, false otherwise
 inline bool exists (const std::string& absolute_filepath) {
     struct stat buffer;   
     return (stat (absolute_filepath.c_str(), &buffer) == 0); 
 }
 
+///> Colorizing std::string for std::cout
+std::string red(const std::string str);
+///> Colorizing std::string for std::cout
+std::string bold_red(const std::string str);
+///> Colorizing std::string for std::cout
+std::string bold_green(const std::string str);
+///> Colorizing std::string for std::cout
+std::string green(const std::string str);
 #endif
