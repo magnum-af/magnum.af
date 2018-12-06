1d519425ccf81548172294baf2b67ec250e647a2
Paul Heistracher <e1225725@student.tuwien.ac.at>
2018-12-06 15:39:00 +0100

    cg energy and gradient

--- src/solvers/cg_minimizer.cpp
+++ src/solvers/cg_minimizer.cpp
@@ -5,23 +5,42 @@ CG_Minimizer::CG_Minimizer()
 }
 
 // Calculation of effective field
-af::array CG_Minimizer::h(const State& state){
-    if( llgterms.size() == 0){
-        std::cout<<"ERROR: minimizer.cpp: Number of llgterms == 0. Please add at least one term to CG_Minimizer.llgterms! Aborting..."<<std::endl;
+af::array CG_Minimizer::Heff(const State& state){
+    if( llgterms_.size() == 0){
+        std::cout<< bold_red("ERROR: LBFGS_Minimizer::Heff: Number of _llgterms == 0. Please add at least one term to LBFGS_Minimizer.llgterms_! Aborting...")<<std::endl;
         exit (EXIT_FAILURE);
      }
     af::timer timer=af::timer::start();
-    af::array solution = llgterms[0]->h(state);
-    for(unsigned i = 1; i < llgterms.size() ; ++i ){
-        solution+=llgterms[i]->h(state);
+    af::array solution = llgterms_[0]->h(state);
+    for(unsigned i = 1; i < llgterms_.size() ; ++i ){
+        solution+=llgterms_[i]->h(state);
     }
-    time_h+=af::timer::stop(timer);
+    time_calc_heff_ += af::timer::stop(timer);
     return solution;
 }
 
-void CG_Minimizer::minimize(State& state){
+double CG_Minimizer::EnergyAndGradient(const State& state, af::array& gradient){
+    if( llgterms_.size() == 0){
+        std::cout<< bold_red("ERROR: LBFGS_Minimizer::Heff: Number of _llgterms == 0. Please add at least one term to LBFGS_Minimizer.llgterms_! Aborting...")<<std::endl;
+        exit (EXIT_FAILURE);
+     }
+    af::timer timer=af::timer::start();
+    //Avoiding array with zeros, starting loop with second term in llgterms
+    af::array h = llgterms_[0]->h(state);
+    double energy = llgterms_[0]->E(state, h);
+    for(unsigned i = 1; i < llgterms_.size() ; ++i ){
+        af::array temp_h = llgterms_[i]->h(state);
+        h+=temp_h;
+        energy+=llgterms_[i]->E(state,temp_h);
+    }
+    gradient = 1./(state.param.mu0 * state.param.ms) * cross4(state.m, cross4(state.m, h)); 
+    time_calc_heff_ += af::timer::stop(timer);
+    return energy;
+}
+
+void CG_Minimizer::Minimize(State& state){
     af::timer timer = af::timer::start();
-    af::print("h in minimize", af::mean(h(state)));//TODO
+    af::print("h in minimize", af::mean(Heff(state)));//TODO
 
     std::cout << "CG_Minimizer: time = " << af::timer::stop(timer) << std::endl;
 }; 
--- src/solvers/cg_minimizer.hpp
+++ src/solvers/cg_minimizer.hpp
@@ -1,28 +1,28 @@
 #ifndef CG_MINIMIZER_H
 #define CG_MINIMIZER_H
-#include <memory>
-#include <list>
-#include <algorithm>
 #include "arrayfire.h"
 #include "../state.hpp"
+#include "../misc.hpp"
+#include "../func.hpp"
 #include "../llg_terms/LLGTerm.hpp"
 
 //For second Method, use interface class:
 //https://stackoverflow.com/questions/40624175/c-how-to-implement-a-switch-between-class-members
 
+//! Preconditioned non-linear conjugate gradient minimizer.
+
+//! Preconditioned Non-Linear Conjugate Gradient Minimizer (courtesy of Thomas Schrefl et.al.)
+//!
 class CG_Minimizer {
     public:
         CG_Minimizer();
-
-        af::array h(const State& m);// Effective Field 
-        void minimize(State&); // Minimization routine
-
-        LlgTerms llgterms;
-
-        double get_time_h() const { return time_h;};
+        void Minimize(State&); // Minimization routine
+        LlgTerms llgterms_;
+        double GetTimeCalcHeff() const { return time_calc_heff_;}; ///< Accumulated time for calculation of Heff.
     private:
-        double time_h{0};
-        
+        af::array Heff(const State& m);///< Effective Field 
+        double EnergyAndGradient(const State& state, af::array& gradient);
+        double time_calc_heff_{0};///< Timer measuring calls to effective field _h
 };
 
 #endif
