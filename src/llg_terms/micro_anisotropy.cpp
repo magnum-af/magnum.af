d3effe265bed897514d4266f43a295be52eaa470
paul <paul.thomas.heistracher@univie.ac.at>
2018-12-10 16:43:21 +0100

    llg_terms/micro_anisotropy.cpp skipping namespace af

--- src/llg_terms/micro_anisotropy.cpp
+++ src/llg_terms/micro_anisotropy.cpp
@@ -2,7 +2,6 @@
 //Hu(r)=2 Ku1 /(mu0 Ms) eu ( eu . m) 
 //With higher order (not implemented): Hu(r)=2 Ku1 /(mu0 Ms) eu ( eu . m) ( + 4 Ku2 /(mu0 Ms) eu ( eu . m)^3
 #include "micro_anisotropy.hpp"
-using namespace af;
 
 //Energy calculation
 //Edemag=-mu0/2 integral(M . Hdemag) dx
@@ -18,20 +17,20 @@ double ANISOTROPY::E(const State& state, const af::array& h){
 ANISOTROPY::ANISOTROPY (Mesh meshin, Param paramin) : param(paramin),mesh(meshin){
   //Normal vector
   double norm=sqrt(pow(param.Ku1_axis[0],2)+ pow(param.Ku1_axis[1],2) + pow(param.Ku1_axis[2], 2));
-  eu=array(mesh.n0,mesh.n1,mesh.n2,3,f64);
-  eu(span,span,span,0)=param.Ku1_axis[0]/norm;
-  eu(span,span,span,1)=param.Ku1_axis[1]/norm;
-  eu(span,span,span,2)=param.Ku1_axis[2]/norm;
+  eu = af::array(mesh.n0,mesh.n1,mesh.n2,3,f64);
+  eu(af::span,af::span,af::span,0)=param.Ku1_axis[0]/norm;
+  eu(af::span,af::span,af::span,1)=param.Ku1_axis[1]/norm;
+  eu(af::span,af::span,af::span,2)=param.Ku1_axis[2]/norm;
 }
 
-array ANISOTROPY::h(const State& state){
-  timer_anisotropy = timer::start();
-  array anisotropy = eu*state.m;
-  anisotropy=sum(anisotropy,3);
-  anisotropy=tile(anisotropy,1,1,1,3);
+af::array ANISOTROPY::h(const State& state){
+  timer_anisotropy = af::timer::start();
+  af::array anisotropy = eu*state.m;
+  anisotropy=af::sum(anisotropy,3);
+  anisotropy=af::tile(anisotropy,1,1,1,3);
 
-  if(param.afsync) sync();
-  cpu_time += timer::stop(timer_anisotropy);
+  if(param.afsync) af::sync();
+  cpu_time += af::timer::stop(timer_anisotropy);
   return  2.* param.Ku1/(param.mu0 * param.ms) * (eu* anisotropy);
 }
 
