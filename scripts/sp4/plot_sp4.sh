84f8c936c3dbe0c1e2abbc061c9b945d24238df9
Paul Heistracher <e1225725@student.tuwien.ac.at>
2019-01-23 22:27:47 +0100

    magnum.af postprocessing added

--- scripts/sp4/plot_sp4.sh
+++ scripts/sp4/plot_sp4.sh
@@ -4,9 +4,8 @@ gnuplot -e 'set terminal pdf enhanced;
             set xlabel "t [ns]";
             set ylabel "average magnetization";
             set title "\muMAG Standard Problem 4";
-            p "m.dat" u ($1*1e9):2 w l t "mx","m.dat" u ($1*1e9):3 w l t "my", "m.dat" u ($1*1e9):4 w l t "mz"'
-
-gnuplot -e 'set xlabel "t [ns]";
-            set ylabel "average magnetization";
-            set title "\muMAG Standard Problem 4";
-            p "m.dat" u ($1*1e9):2 w l t "mx","m.dat" u ($1*1e9):3 w l t "my", "m.dat" u ($1*1e9):4 w l t "mz"' -persist 
+            p "m.dat" u ($1*1e9):2 w l t "mx","m.dat" u ($1*1e9):3 w l t "my", "m.dat" u ($1*1e9):4 w l t "mz";
+            set terminal pop;
+            set output;
+            p "m.dat" u ($1*1e9):2 w l t "mx","m.dat" u ($1*1e9):3 w l t "my", "m.dat" u ($1*1e9):4 w l t "mz";
+            ' -persist
