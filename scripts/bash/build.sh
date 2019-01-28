711e8d7cd9cf5829cbc9818a7a95d3c8fa34c2ca
paul <e1225725>
2019-01-28 11:48:45 +0100

    cleanup build, i.e. removing GTO user name

--- scripts/bash/build.sh
+++ scripts/bash/build.sh
@@ -14,15 +14,7 @@ then
 fi
 cd $1/build
 
-if [ "$HOSTNAME" = SERVERGPU1 ]; then
-    if [ "$2" == "true" ]; then 
-        /home/paul/Programs/cmake-3.13.0-rc2-Linux-x86_64/bin/cmake -DVTK_DIR:PATH=/home/paul/Programs/VKT-build -DArrayFire_DIR=/usr/local/arrayfire/share/ArrayFire/cmake ..
-    else
-        /home/paul/Programs/cmake-3.13.0-rc2-Linux-x86_64/bin/cmake -DVTK_DIR:PATH=/home/paul/Programs/VKT-build -DArrayFire_DIR=/usr/local/arrayfire/share/ArrayFire/cmake .. > /dev/null
-    fi
-else
-    [ "$2" == "true" ] && cmake .. || cmake .. > /dev/null
-fi
+[ "$2" == "true" ] && cmake .. || cmake .. > /dev/null
 
 [ "$2" == "true" ] && make -j || make -j > /dev/null
 cd $currdir
