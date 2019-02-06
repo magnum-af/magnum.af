87964fb61c05b9bf30d02e2221b5cb543fbae6fb
paul <e1225725>
2019-02-06 13:48:39 +0100

    Dockerfile.cpu update

--- tests/runall.sh
+++ tests/runall.sh
@@ -6,11 +6,17 @@ $1/tests/unit/cpp/runall.sh $1
 $1/tests/integration/cpp/runall.sh $1
 
 # py unit
+command -v pip3 >/dev/null 2>&1 && pip3_output="$(pip3 show arrayfire)" # Note: output of 'pip3 show' is empty if package not found
+if [ -n "$pip3_output" ]; then
+    python="python3"
+else
+    python="python"
+fi
 for filename in $1/tests/unit/*.py; do
-    PYTHONPATH=$1/build/src/ python $filename
+    PYTHONPATH=$1/build/src/ $python $filename
 done
 
 # py integration
 for filename in $1/tests/integration/*.py; do
-    PYTHONPATH=$1/build/src/ python $filename
+    PYTHONPATH=$1/build/src/ $python $filename
 done
