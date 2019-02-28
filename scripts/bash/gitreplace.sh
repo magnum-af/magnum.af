1a30d6be37cf642e758a6891f83e2bb4e8852c9b
paul <paul.thomas.heistracher@univie.ac.at>
2019-02-28 13:32:58 +0100

    udate gitreplace.sh

--- scripts/bash/gitreplace.sh
+++ scripts/bash/gitreplace.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # $1 old pattern
 # $2 new string
-echo "running: git grep -l "$1" | xargs sed -i "s/$1/$2/g""
+# Example: gitreplace.sh \\bOldWord\\b NewWord
+# Note   : for replacing exact words (and not every pattern) use '\\b' (i.e. '\'-escaped '\b').
+set -x
 git grep -l "$1" | xargs sed -i "s/$1/$2/g"
