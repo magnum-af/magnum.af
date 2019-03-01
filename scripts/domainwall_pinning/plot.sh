39754a47352ead54e7c5cfc21daad85b5caca576
paul <paul.thomas.heistracher@univie.ac.at>
2019-03-01 15:32:31 +0100

    domainwall_pinning/plot use pdf terminal

--- scripts/domainwall_pinning/plot.sh
+++ scripts/domainwall_pinning/plot.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 gnuplot -e "
+    set terminal pdf;
+    set output 'm_over_t.pdf';
     set xlabel 't';
     p 'm.dat' u 1:2 w l, 'm.dat' u 1:3 w l, 'm.dat' u 1:4 w l, 'm.dat' u 1:(\$5*4*3.14*1e-7) w l;
 " --persist
 
 gnuplot -e "
+    set terminal pdf;
+    set output 'm_over_hx.pdf';
     set xlabel 'mu0 Hx';
     set ylabel 'm_x';
     p 'm.dat' u (\$5*4*3.14*1e-7):2 w l
