d20841cc37499dea674ad61700d3dccb8ff326d6
paul <paul.thomas.heistracher@univie.ac.at>
2019-03-04 12:21:28 +0100

    constants.hpp

---
+++ src/constants.hpp
@@ -0,0 +1,17 @@
+#ifndef Constants_H
+#define Constants_H
+
+namespace constants{
+    const double mu0 = 1.2566370614e-6; ///< magnetic constant mu_0
+  
+    const double gamma = 2.21276157e5; ///< gyromagnetic ratio gamma
+  
+    const double mu_b = 9.2740154e-24; ///< Bohr magneton mu_bohr
+  
+    const double e = - 1.602176487e-19; ///< elementary charge e
+  
+    const double kb = 1.380648813e-23; ///< Boltzmann constant kb
+  
+    const double hbar = 1.0545718e-34; ///< reduced Planck constant
+}
+#endif
