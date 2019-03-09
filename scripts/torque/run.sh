5f921ad422b6d81be317d428b32340b463d54716
paul <e1225725>
2019-03-09 02:35:45 +0100

    docker t flag

--- scripts/torque/run.sh
+++ scripts/torque/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-simtime=100 # in [ns]
+simtime=100.0 # in [ns]
 nstep=1000 # mean every nth step
-../magnum.af -fs -p plot.gnu offset_sensor.py output "$simtime" "$nstep"
+
+../magnum.af -sg 3 -p plot.gnu offset_sensor.py "$1" "$simtime" "$nstep"
+
+## or use docker
+#../magnum.af.docker -dtg 3 offset_sensor.py "$1" "$simtime" "$nstep"
