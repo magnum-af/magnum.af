9c48229ba1e0cb3a873b6310e6a39371d6fe3cfc
paul <paul.thomas.heistracher@univie.ac.at>
2019-04-05 12:31:12 +0200

    expanded nonequi_demag.cpp unit tests

--- tests/unit/cpp/nonequi_demag.cpp
+++ tests/unit/cpp/nonequi_demag.cpp
@@ -8,9 +8,9 @@
 // Exemplary unit test
 TEST(NonEquiDemagNxxNxyNearTest, n) {
     double Nxx_non = newell::Nxx_nonequi(1, 2, 3, 1, 2, 3, 1, 2, 3);
-    double Nxx_f = Nxxf(1, 2, 3, 1, 2, 3);
+    double Nxx_f = newell::Nxxf(1, 2, 3, 1, 2, 3);
     double Nxy_non = newell::Nxy_nonequi(1, 2, 3, 1, 2, 3, 1, 2, 3);
-    double Nxyg = Nxxg(1, 2, 3, 1, 2, 3);
+    double Nxyg = newell::Nxxg(1, 2, 3, 1, 2, 3);
 
     double Nxx_rel_diff = fabs(2*(Nxx_non-Nxx_f)/(Nxx_non+Nxx_f));
     double Nxx_abs_diff = fabs(Nxx_non-Nxx_f);
@@ -27,9 +27,9 @@ TEST(NonEquiDemagNxxNxyNearTest, n) {
 
 TEST(NonEquiDemagNxxNxyFarTest, n) {
     double Nxx_non = newell::Nxx_nonequi(1, 2, 300, 1, 2, 3, 1, 2, 3);
-    double Nxx_f = Nxxf(1, 2, 300, 1, 2, 3);
+    double Nxx_f = newell::Nxxf(1, 2, 300, 1, 2, 3);
     double Nxy_non = newell::Nxy_nonequi(1, 2, 300, 1, 2, 3, 1, 2, 3);
-    double Nxyg = Nxxg(1, 2, 300, 1, 2, 3);
+    double Nxyg = newell::Nxxg(1, 2, 300, 1, 2, 3);
 
     double Nxx_rel_diff = fabs(2*(Nxx_non-Nxx_f)/(Nxx_non+Nxx_f));
     double Nxx_abs_diff = fabs(Nxx_non-Nxx_f);
@@ -52,6 +52,77 @@ TEST(NonEquiDemagNxxNxyFarTest, n) {
     //std::cout << "Nxx_nonequi: " << Nxx_non << std::endl;
     //std::cout << "Nxx        : " << Nxx_f << std::endl;
     //std::cout << "Nxx_diff   : " << 2*(Nxx_non-Nxx_f)/(Nxx_non+Nxx_f) << std::endl << std::endl;
+}
+
+TEST(NonEquiDemagUnitCubeTest, n) {
+    const double dx = 1;    //Unit cube edge length
+    const int  ix = 2; //Distance between cubes
+    double Nxx = newell::Nxxf(ix, 0, 0, dx, dx, dx);
+    double Nxx_ne = newell::Nxx_nonequi(ix, 0, 0, dx, dx, dx, dx, dx, dx);
+    EXPECT_NEAR(Nxx, Nxx_ne, 1e-16);
+}
+
+//Testing layout in x: #|##
+TEST(NonEquiDemagTwoCubesVersusOneCubeTest_x, n) {
+    const double a = 1;    //Unit cube edge length
+    const int ix = 1; //Distance between cubes
+    const double Nxx_1 = newell::Nxxf(  ix, 0, 0, a, a, a);
+    const double Nxx_2 = newell::Nxxf(2*ix, 0, 0, a, a, a);
+    const double Nxx = Nxx_1 + Nxx_2;
+    const double Nxx_ne = newell::Nxx_nonequi(ix, 0, 0, a, a, a, 2*a, a, a);
+    EXPECT_NEAR(Nxx, Nxx_ne, 1e-16);
+}
+    //std::cout << Nxx_1 << std::endl;
+    //std::cout << Nxx_2 << std::endl;
+    //std::cout << Nxx << std::endl;
+    //std::cout << Nxx_ne << std::endl;
+    //const double dX = 2*dx; //Double unit cube long edge length
+    //const double  x = 2*dx; //Distance between cubes
+
+//Testing layout in y: #|##
+TEST(NonEquiDemagTwoCubesVersusOneCubeTest_y, n) {
+    const double a = 1;    //Unit cube edge length
+    const int iy = 1; //Distance between cubes
+    const double Nxx_1 = newell::Nxxf(0,   iy, 0, a, a, a);
+    const double Nxx_2 = newell::Nxxf(0, 2*iy, 0, a, a, a);
+    const double Nxx = Nxx_1 + Nxx_2;
+    const double Nxx_ne = newell::Nxx_nonequi(0, iy, 0, a, a, a, a, 2*a, a);
+    EXPECT_NEAR(Nxx, Nxx_ne, 1e-15);
+}
+
+//Testing layout in z: #|##
+TEST(NonEquiDemagTwoCubesVersusOneCubeTest_z, n) {
+    const double a = 1;    //Unit cube edge length
+    const int iz = 1; //Distance between cubes
+    const double Nxx_1 = newell::Nxxf(0,   iz, 0, a, a, a);
+    const double Nxx_2 = newell::Nxxf(0, 2*iz, 0, a, a, a);
+    const double Nxx = Nxx_1 + Nxx_2;
+    const double Nxx_ne = newell::Nxx_nonequi(0, iz, 0, a, a, a, a, 2*a, a);
+    EXPECT_NEAR(Nxx, Nxx_ne, 1e-15);
+}
+
+//                      |#
+//Testing layout in x: #|#
+TEST(NonEquiDemagTwoCubesVersusOneCubeTest_xy, n) {
+    const double a = 1;    //Unit cube edge length
+    const int ixy = 1; //Distance between cubes
+    const double Nxx_1 = newell::Nxxf(ixy,   ixy, 0, a, a, a);
+    const double Nxx_2 = newell::Nxxf(ixy, 2*ixy, 0, a, a, a);
+    const double Nxx = Nxx_1 + Nxx_2;
+    const double Nxx_ne = newell::Nxx_nonequi(ixy, ixy, 0, a, a, a, a, 2*a, a);
+    EXPECT_NEAR(Nxx, Nxx_ne, 1e-15);
+}
+
+//Testing layout in x: #|#
+//                      |#
+TEST(NonEquiDemagTwoCubesVersusOneCubeTest_x_minus_y, n) {
+    const double a = 1;    //Unit cube edge length
+    const int ixy = 1; //Distance between cubes
+    const double Nxx_1 = newell::Nxxf(ixy, -  ixy, 0, a, a, a);
+    const double Nxx_2 = newell::Nxxf(ixy, -2*ixy, 0, a, a, a);
+    const double Nxx = Nxx_1 + Nxx_2;
+    const double Nxx_ne = newell::Nxx_nonequi(ixy, -2*ixy, 0, a, a, a, a, 2*a, a);
+    EXPECT_NEAR(Nxx, Nxx_ne, 1e-15);
 }
 
 int main(int argc, char **argv) {
