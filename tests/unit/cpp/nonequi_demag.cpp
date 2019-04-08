0cd2697a6098d5d07fcbc2b8cb00402699826f32
paul <paul.thomas.heistracher@univie.ac.at>
2019-04-08 17:37:56 +0200

    nonequi_demag.cpp adding namespace change

--- tests/unit/cpp/nonequi_demag.cpp
+++ tests/unit/cpp/nonequi_demag.cpp
@@ -7,10 +7,10 @@
  
 // Exemplary unit test
 TEST(NonEquiDemagNxxNxyNearTest, n) {
-    double Nxx_non = newell::Nxx_nonequi(1, 2, 3, 1, 2, 3, 1, 2, 3);
-    double Nxx_f = newell::Nxxf(1, 2, 3, 1, 2, 3);
-    double Nxy_non = newell::Nxy_nonequi(1, 2, 3, 1, 2, 3, 1, 2, 3);
-    double Nxyg = newell::Nxxg(1, 2, 3, 1, 2, 3);
+    double Nxx_non = newell_nonequi::Nxx_nonequi(1, 2, 3, 1, 2, 3, 1, 2, 3);
+    double Nxx_f = newell::Nxx(1, 2, 3, 1, 2, 3);
+    double Nxy_non = newell_nonequi::Nxy_nonequi(1, 2, 3, 1, 2, 3, 1, 2, 3);
+    double Nxyg = newell::Nxy(1, 2, 3, 1, 2, 3);
 
     EXPECT_NEAR(Nxx_non, Nxx_f, 1e-14);
     double Nxx_rel_diff = fabs(2*(Nxx_non-Nxx_f)/(Nxx_non+Nxx_f));
@@ -22,10 +22,10 @@ TEST(NonEquiDemagNxxNxyNearTest, n) {
 }
 
 TEST(NonEquiDemagNxxNxyFarTest, n) {
-    double Nxx_non = newell::Nxx_nonequi(1, 2, 300, 1, 2, 3, 1, 2, 3);
-    double Nxx_f = newell::Nxxf(1, 2, 300, 1, 2, 3);
-    double Nxy_non = newell::Nxy_nonequi(1, 2, 300, 1, 2, 3, 1, 2, 3);
-    double Nxyg = newell::Nxxg(1, 2, 300, 1, 2, 3);
+    double Nxx_non = newell_nonequi::Nxx_nonequi(1, 2, 300, 1, 2, 3, 1, 2, 3);
+    double Nxx_f = newell::Nxx(1, 2, 300, 1, 2, 3);
+    double Nxy_non = newell_nonequi::Nxy_nonequi(1, 2, 300, 1, 2, 3, 1, 2, 3);
+    double Nxyg = newell::Nxy(1, 2, 300, 1, 2, 3);
 
     double Nxx_rel_diff = fabs(2*(Nxx_non-Nxx_f)/(Nxx_non+Nxx_f));
     double Nxx_abs_diff = fabs(Nxx_non-Nxx_f);
@@ -43,8 +43,8 @@ TEST(NonEquiDemagNxxNxyFarTest, n) {
 TEST(NonEquiDemagUnitCubeTest, n) {
     const double a = 1;
     const int  ix = 2;
-    double Nxx = newell::Nxxf(ix, 0, 0, a, a, a);
-    double Nxx_ne = newell::Nxx_nonequi(ix, 0, 0, a, a, a, a, a, a);
+    double Nxx = newell::Nxx(ix, 0, 0, a, a, a);
+    double Nxx_ne = newell_nonequi::Nxx_nonequi(ix, 0, 0, a, a, a, a, a, a);
     EXPECT_NEAR(Nxx, Nxx_ne, 1e-16);
 }
 
@@ -52,10 +52,10 @@ TEST(NonEquiDemagUnitCubeTest, n) {
 TEST(NonEquiDemagTwoCubesVersusOneCubeTest_x, n) {
     const double a = 1;
     const int ix = 1;
-    const double Nxx_1 = newell::Nxxf(  ix, 0, 0, a, a, a);
-    const double Nxx_2 = newell::Nxxf(2*ix, 0, 0, a, a, a);
+    const double Nxx_1 = newell::Nxx(  ix, 0, 0, a, a, a);
+    const double Nxx_2 = newell::Nxx(2*ix, 0, 0, a, a, a);
     const double Nxx = Nxx_1 + Nxx_2;
-    const double Nxx_ne = newell::Nxx_nonequi(ix, 0, 0, a, a, a, 2*a, a, a);
+    const double Nxx_ne = newell_nonequi::Nxx_nonequi(ix, 0, 0, a, a, a, 2*a, a, a);
     EXPECT_NEAR(Nxx, Nxx_ne, 1e-16);
 }
 
@@ -63,10 +63,10 @@ TEST(NonEquiDemagTwoCubesVersusOneCubeTest_x, n) {
 TEST(NonEquiDemagTwoCubesVersusOneCubeTest_y, n) {
     const double a = 1;
     const int iy = 1;
-    const double Nxx_1 = newell::Nxxf(0,   iy, 0, a, a, a);
-    const double Nxx_2 = newell::Nxxf(0, 2*iy, 0, a, a, a);
+    const double Nxx_1 = newell::Nxx(0,   iy, 0, a, a, a);
+    const double Nxx_2 = newell::Nxx(0, 2*iy, 0, a, a, a);
     const double Nxx = Nxx_1 + Nxx_2;
-    const double Nxx_ne = newell::Nxx_nonequi(0, iy, 0, a, a, a, a, 2*a, a);
+    const double Nxx_ne = newell_nonequi::Nxx_nonequi(0, iy, 0, a, a, a, a, 2*a, a);
     EXPECT_NEAR(Nxx, Nxx_ne, 1e-15);
 }
 
@@ -74,10 +74,10 @@ TEST(NonEquiDemagTwoCubesVersusOneCubeTest_y, n) {
 TEST(NonEquiDemagTwoCubesVersusOneCubeTest_z, n) {
     const double a = 1;
     const int iz = 1;
-    const double Nxx_1 = newell::Nxxf(0,   iz, 0, a, a, a);
-    const double Nxx_2 = newell::Nxxf(0, 2*iz, 0, a, a, a);
+    const double Nxx_1 = newell::Nxx(0,   iz, 0, a, a, a);
+    const double Nxx_2 = newell::Nxx(0, 2*iz, 0, a, a, a);
     const double Nxx = Nxx_1 + Nxx_2;
-    const double Nxx_ne = newell::Nxx_nonequi(0, iz, 0, a, a, a, a, 2*a, a);
+    const double Nxx_ne = newell_nonequi::Nxx_nonequi(0, iz, 0, a, a, a, a, 2*a, a);
     EXPECT_NEAR(Nxx, Nxx_ne, 1e-15);
 }
 
@@ -86,10 +86,10 @@ TEST(NonEquiDemagTwoCubesVersusOneCubeTest_z, n) {
 TEST(NonEquiDemagTwoCubesVersusOneCubeTest_xy, n) {
     const double a = 1;
     const int ixy = 1;
-    const double Nxx_1 = newell::Nxxf(ixy,   ixy, 0, a, a, a);
-    const double Nxx_2 = newell::Nxxf(ixy, 2*ixy, 0, a, a, a);
+    const double Nxx_1 = newell::Nxx(ixy,   ixy, 0, a, a, a);
+    const double Nxx_2 = newell::Nxx(ixy, 2*ixy, 0, a, a, a);
     const double Nxx = Nxx_1 + Nxx_2;
-    const double Nxx_ne = newell::Nxx_nonequi(ixy, ixy, 0, a, a, a, a, 2*a, a);
+    const double Nxx_ne = newell_nonequi::Nxx_nonequi(ixy, ixy, 0, a, a, a, a, 2*a, a);
     EXPECT_NEAR(Nxx, Nxx_ne, 1e-15);
 }
 
@@ -98,10 +98,10 @@ TEST(NonEquiDemagTwoCubesVersusOneCubeTest_xy, n) {
 TEST(NonEquiDemagTwoCubesVersusOneCubeTest_x_minus_y, n) {
     const double a = 1;
     const int ixy = 1;
-    const double Nxx_1 = newell::Nxxf(ixy, -  ixy, 0, a, a, a);
-    const double Nxx_2 = newell::Nxxf(ixy, -2*ixy, 0, a, a, a);
+    const double Nxx_1 = newell::Nxx(ixy, -  ixy, 0, a, a, a);
+    const double Nxx_2 = newell::Nxx(ixy, -2*ixy, 0, a, a, a);
     const double Nxx = Nxx_1 + Nxx_2;
-    const double Nxx_ne = newell::Nxx_nonequi(ixy, -2*ixy, 0, a, a, a, a, 2*a, a);
+    const double Nxx_ne = newell_nonequi::Nxx_nonequi(ixy, -2*ixy, 0, a, a, a, a, 2*a, a);
     EXPECT_NEAR(Nxx, Nxx_ne, 1e-15);
 }
 
