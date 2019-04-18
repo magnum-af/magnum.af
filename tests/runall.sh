bfbe014b1379a4dcee0396e1fff58c37ef1ad959
paul <paul.thomas.heistracher@univie.ac.at>
2019-04-18 15:06:57 +0200

    tests/runall.sh add path

--- tests/runall.sh
+++ tests/runall.sh
@@ -2,5 +2,5 @@
 #usage: ./runall /path/to/gitdirectory(magnum.af)
 
 # cpp
-./unit/run_all_unit_tests.sh $1
-./integration/run_all_integration_tests.sh $1
+$1/tests/unit/run_all_unit_tests.sh $1
+$1/tests/integration/run_all_integration_tests.sh $1
