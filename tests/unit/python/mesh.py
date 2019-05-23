c06e0742a90271f9a4643b5c68a18f733d5755c3
paul <paul.thomas.heistracher@univie.ac.at>
2019-05-23 12:12:08 +0200

    mesh.py fix unittest n0 -> nx

--- tests/unit/python/mesh.py
+++ tests/unit/python/mesh.py
@@ -3,39 +3,39 @@ import arrayfire as af
 from magnumaf import Mesh
 
 class MeshTest(unittest.TestCase):
-    n0 = 100
-    n1 = 25
-    n2 = 1
+    nx = 100
+    ny = 25
+    nz = 1
     dx = 5.e-7/100.
     dy = 1.25e-7/25.
     dz = 3.e-9
 
     def test_mesh_getter(self):
-        mesh = Mesh(self.n0, self.n1, self.n2, self.dx, self.dy, self.dz)
-        self.assertEqual(self.n0, mesh.n0)
-        self.assertEqual(self.n1, mesh.n1)
-        self.assertEqual(self.n2, mesh.n2)
+        mesh = Mesh(self.nx, self.ny, self.nz, self.dx, self.dy, self.dz)
+        self.assertEqual(self.nx, mesh.nx)
+        self.assertEqual(self.ny, mesh.ny)
+        self.assertEqual(self.nz, mesh.nz)
         self.assertEqual(self.dx, mesh.dx)
         self.assertEqual(self.dy, mesh.dy)
         self.assertEqual(self.dz, mesh.dz)
 
     def test_mesh_setter_using_getter(self):
-        n0 = 4*100
-        n1 = 4*25
-        n2 = 4*1
+        nx = 4*100
+        ny = 4*25
+        nz = 4*1
         dx = 4*5.e-7/100.
         dy = 4*1.25e-7/25.
         dz = 4*3.e-9
-        mesh = Mesh(self.n0, self.n1, self.n2, self.dx, self.dy, self.dz)
-        mesh.n0 = n0
-        mesh.n1 = n1
-        mesh.n2 = n2
+        mesh = Mesh(self.nx, self.ny, self.nz, self.dx, self.dy, self.dz)
+        mesh.nx = nx
+        mesh.ny = ny
+        mesh.nz = nz
         mesh.dx = dx
         mesh.dy = dy
         mesh.dz = dz
-        self.assertEqual(n0, mesh.n0)
-        self.assertEqual(n1, mesh.n1)
-        self.assertEqual(n2, mesh.n2)
+        self.assertEqual(nx, mesh.nx)
+        self.assertEqual(ny, mesh.ny)
+        self.assertEqual(nz, mesh.nz)
         self.assertEqual(dx, mesh.dx)
         self.assertEqual(dy, mesh.dy)
         self.assertEqual(dz, mesh.dz)
