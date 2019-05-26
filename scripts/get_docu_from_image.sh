42f23ead9f27eab14ca0fd3f4f2cc06949babf49
paul <paul.thomas.heistracher@univie.ac.at>
2019-05-26 20:26:27 +0200

    edit scripts/get_docu_from_image.sh

--- scripts/get_docu_from_image.sh
+++ scripts/get_docu_from_image.sh
@@ -1,4 +1,13 @@
+#!/bin/bash -e
 id=$(docker create magnum.af.cpu-dev)
-docker cp $id:/home/magnum.af/html/ - > magnum.af_documentation_html.tar
-docker cp $id:/home/magnum.af/latex/ - > magnum.af_documentation_latex.tar
+docker cp $id:/home/magnum.af/docu/html/ - > html.tar
+docker cp $id:/home/magnum.af/docu/latex/ - > latex.tar
 docker rm -v $id
+
+tar -xf html.tar
+rm html.tar
+echo "extracted $PWD/html/"
+
+tar -xf latex.tar
+rm latex.tar
+echo "extracted $PWD/latex/"
