6a16e873186461fee29a4f782962c2bb5a4b265e
paul <paul.thomas.heistracher@univie.ac.at>
2019-05-27 16:07:24 +0200

    domainwall_pinning_x.py update to API v1

--- scripts/domainwall_pinning/domainwall_pinning_x.py
+++ scripts/domainwall_pinning/domainwall_pinning_x.py
@@ -47,16 +47,6 @@ m[nx/2:,:,:,0] = af.constant(-1.0, int(nx/2) , ny, nz, 1, dtype=af.Dtype.f64)
 m[nx/2:,:,:,1] = af.constant( 0.3, int(nx/2) , ny, nz, 1, dtype=af.Dtype.f64)
 
 mesh = Mesh(nx, ny, nz, x/nx, y/ny, z/nz)
-material = Material(alpha=1.0, Ku1_axis=[1., 0., 0.])
-state = State(mesh, material, m)
-state.normalize()
-state.write_vti(sys.argv[1] + "minit")
-#m = af.constant(0.0, nx, ny, nz, 3, dtype=af.Dtype.f64)
-#m[:nx/2-5,:,:,0] = af.constant(1.0, int(nx/2)-5 , ny, nz, 1, dtype=af.Dtype.f64)
-#m[nx/2-5:nx/2+5,:,:,2] = af.constant(1.0, 10 , ny, nz, 1, dtype=af.Dtype.f64)
-#m[nx/2+5:,:,:,0] = af.constant(-1.0, int(nx/2)-5 , ny, nz, 1, dtype=af.Dtype.f64)
-#print ("int(nx/2)=",nx/2)
-#print(m)
 
 # Setting A values as field
 A_field = af.constant(0.0, nx, ny, nz, 3, dtype=af.Dtype.f64)
@@ -79,43 +69,15 @@ Ku1_field[:nx/2,:,:,:] = af.constant(soft_K_uni, int(nx/2) , ny, nz, 3, dtype=af
 # hard
 Ku1_field[nx/2:,:,:,:] = af.constant(hard_K_uni, int(nx/2) , ny, nz, 3, dtype=af.Dtype.f64)
 
-#print ("material:", material.alpha, state.Ms, material.A, material.Ku1, material.Ku1_axis)
-#TODO# string: setting ms=0 or not setting ms leads to segfault!: #material = Material(alpha=1.0, ms=0, Ku1_axis=[1., 0., 0.])
-#also not setting ms: material = Material(alpha=1.0, Ku1_axis=[1., 0., 0.])
-
-#print (state.m)
-state.micro_A_field = A_field
-state.micro_Ms_field = Ms_field
-state.micro_Ku1_field = Ku1_field
-#state.write_vti(sys.argv[1] + "m_init")
-#print (Ms_field)
-#print (state.micro_Ms_field)
-#print (Ku1_field)
-#print (state.micro_Ku1_field)
-#print (A_field)
-#print (state.micro_A_field)
+state = State(mesh, Ms_field, m)
+state.normalize()
+state.write_vti(sys.argv[1] + "minit")
 fields = [
     ExternalField(af.constant(0.0, nx, ny, nz, 3, dtype=af.Dtype.f64)),
-    ExchangeField(state.mesh, state.material),
-    UniaxialAnisotropyField(state.mesh, state.material),
+    ExchangeField(A_field),
+    UniaxialAnisotropyField(Ku1_field, Ku1_axis=[1., 0., 0.]),
 ]
-Llg = LLGIntegrator(terms=fields)
-
-#stream = open(sys.argv[1]+"m_relax.dat", "w")
-#print("Relaxing 2.2e-10 [s]")
-#state.write_vti(sys.argv[1] + "m_init")
-#i = 0
-#while state.t < 2.2e-10: #value for 100x6x6 geometry
-#  if i%200 == 0:
-#    state.write_vti(sys.argv[1] + "m_relax" + str(i))
-#  i = i + 1
-#  Llg.step(state)
-#  mean = af.mean(af.mean(af.mean(state.m, dim=0), dim=1), dim=2)
-#  stream.write("%e, %e, %e, %e\n" %(state.t, mean[0,0,0,0].scalar(), mean[0,0,0,1].scalar(), mean[0,0,0,2].scalar()))
-#  #stream.write("%e, %e, %e, %e, %e\n" %(state.t, state.m_mean(0), state.m_mean(1), state.m_mean(2), -state.t/50e-9/Constants.mu0))
-#  stream.flush()
-#state.write_vti(sys.argv[1] + "m_relax")
-#stream.close()
+Llg = LLGIntegrator(alpha=1.0, terms=fields)
 
 fastenup = 10
 print("Start 100 [ns] hysteresis")
@@ -125,12 +87,12 @@ i = 0
 while (state.t < 1e-7/fastenup and state.m_mean(0) < (1. - 1e-6)):
   if i%2000 == 0:
     state.write_vti(sys.argv[1] + "m_" + str(i))
-  fields[0].set_homogenuous_field(state, fastenup * state.t/50e-9/Constants.mu0, 0.0, 0.0)
+  fields[0].set_homogenuous_field(fastenup * state.t/50e-9/Constants.mu0, 0.0, 0.0)
   Llg.step(state)
   #mean = af.mean(af.mean(af.mean(state.m, dim=0), dim=1), dim=2)
-  printzee = af.mean(af.mean(af.mean(fields[0].get_zee(), dim=0), dim=1), dim=2)
+  printzee = af.mean(af.mean(af.mean(fields[0].h(state), dim=0), dim=1), dim=2)
   print(state.t, state.m_mean(0), state.m_mean(1), state.m_mean(2), state.t/50e-9/Constants.mu0, printzee[0,0,0,0].scalar()*Constants.mu0)
-  printzee = af.mean(af.mean(af.mean(fields[0].get_zee(), dim=0), dim=1), dim=2)
+  #printzee = af.mean(af.mean(af.mean(fields[0].get_zee(), dim=0), dim=1), dim=2)
   stream.write("%e, %e, %e, %e, %e, %e\n" %(state.t, state.m_mean(0), state.m_mean(1), state.m_mean(2), state.t/50e-9/Constants.mu0, printzee[0,0,0,0].scalar()))
   stream.flush()
   i = i + 1
--- scripts/domainwall_pinning/domainwall_pinning_z.py
+++ scripts/domainwall_pinning/domainwall_pinning_z.py
@@ -47,18 +47,6 @@ m[:,:,nz/2:,2] = af.constant(-1.0, nx, ny, int(nz/2), 1, dtype=af.Dtype.f64)
 m[:,:,nz/2:,1] = af.constant( 0.3, nx, ny, int(nz/2), 1, dtype=af.Dtype.f64)
 
 mesh = Mesh(nx, ny, nz, x/nx, y/ny, z/nz)
-material = Material(alpha=1.0, ms=0, Ku1_axis=[0, 0, 1])
-state = State(mesh, material, m)
-state.normalize()
-state.write_vti(sys.argv[1] + "minit")
-#print("m", state.m)
-
-#m = af.constant(0.0, nx, ny, nz, 3, dtype=af.Dtype.f64)
-#m[:nx/2-5,:,:,0] = af.constant(1.0, int(nx/2)-5 , ny, nz, 1, dtype=af.Dtype.f64)
-#m[nx/2-5:nx/2+5,:,:,2] = af.constant(1.0, 10 , ny, nz, 1, dtype=af.Dtype.f64)
-#m[nx/2+5:,:,:,0] = af.constant(-1.0, int(nx/2)-5 , ny, nz, 1, dtype=af.Dtype.f64)
-#print ("int(nx/2)=",nx/2)
-#print(m)
 
 # Setting A values as field
 A_field = af.constant(0.0, nx, ny, nz, 3, dtype=af.Dtype.f64)
@@ -81,55 +69,19 @@ Ku1_field[:,:,:nz/2,:] = af.constant(soft_K_uni, nx, ny, int(nz/2), 3, dtype=af.
 # hard                
 Ku1_field[:,:,nz/2:,:] = af.constant(hard_K_uni, nx, ny, int(nz/2), 3, dtype=af.Dtype.f64)
 
-#print ("material:", material.alpha, state.Ms, material.A, material.Ku1, material.Ku1_axis)
 #TODO# string: setting ms=0 or not setting ms leads to segfault!: #material = Material(alpha=1.0, ms=0, Ku1_axis=[1., 0., 0.])
 #also not setting ms: material = Material(alpha=1.0, Ku1_axis=[1., 0., 0.])
 
-#print (state.m)
-state.micro_A_field = A_field
-state.micro_Ms_field = Ms_field
-state.micro_Ku1_field = Ku1_field
-#print("Afield*1e12", 1e12*state.micro_A_field)
-#print(state.micro_Ms_field)
-#print(state.micro_Ku1_field)
+state = State(mesh, Ms_field, m)
+state.normalize()
+state.write_vti(sys.argv[1] + "minit")
 
-#temp_workaround = State(mesh, material, state.micro_A_field)
-#temp_workaround.write_vti(sys.argv[1] +"state.micro_A_field")
-#temp_workaround = State(mesh, material, state.micro_Ms_field)
-#temp_workaround.write_vti(sys.argv[1] +"state.micro_Ms_field")
-#temp_workaround = State(mesh, material, state.micro_Ku1_field)
-#temp_workaround.write_vti(sys.argv[1] +"state.micro_Ku1_field")
-
-#state.write_vti(sys.argv[1] + "m_init")
-#print (Ms_field)
-#print (state.micro_Ms_field)
-#print (Ku1_field)
-#print (state.micro_Ku1_field)
-#print (A_field)
-#print (state.micro_A_field)
 fields = [
     ExternalField(af.constant(0.0, nx, ny, nz, 3, dtype=af.Dtype.f64)),
-    ExchangeField(state.mesh, state.material),
-    UniaxialAnisotropyField(state.mesh, state.material),
-    #DemagField(mesh, material),
+    ExchangeField(A_field),
+    UniaxialAnisotropyField(Ku1_field),
 ]
-Llg = LLGIntegrator(terms=fields)
-
-#stream = open(sys.argv[1]+"m_relax.dat", "w")
-#print("Relaxing 2.2e-10 [s]")
-#state.write_vti(sys.argv[1] + "m_init")
-#i = 0
-#while state.t < 2.2e-10: #value for 100x6x6 geometry
-#  if i%200 == 0:
-#    state.write_vti(sys.argv[1] + "m_relax" + str(i))
-#  i = i + 1
-#  Llg.step(state)
-#  mean = af.mean(af.mean(af.mean(state.m, dim=0), dim=1), dim=2)
-#  stream.write("%e, %e, %e, %e\n" %(state.t, mean[0,0,0,0].scalar(), mean[0,0,0,1].scalar(), mean[0,0,0,2].scalar()))
-#  #stream.write("%e, %e, %e, %e, %e\n" %(state.t, state.m_mean(0), state.m_mean(1), state.m_mean(2), -state.t/50e-9/Constants.mu0))
-#  stream.flush()
-#state.write_vti(sys.argv[1] + "m_relax")
-#stream.close()
+Llg = LLGIntegrator(alpha=1.0, terms=fields)
 
 fastenup = 10
 print("Start [ns] hysteresis")
@@ -137,7 +89,7 @@ stream = open(sys.argv[1]+"m.dat", "w")
 timer = time.time()
 i = 0
 
-printzee = af.mean(af.mean(af.mean(fields[0].get_zee(), dim=0), dim=1), dim=2)
+printzee = af.mean(af.mean(af.mean(fields[0].h(state), dim=0), dim=1), dim=2)
 #print("m", state.m)
 #print(state.t, state.m_mean(0), state.m_mean(1), state.m_mean(2), fastenup * state.t/50e-9/Constants.mu0, printzee[0,0,0,0].scalar())
 #while i < 10:
@@ -145,9 +97,9 @@ while (state.t < 1e-7/fastenup and state.m_mean(2) < (1. - 1e-6)):
   #print("m", state.m)
   if i%2000 == 0:
     state.write_vti(sys.argv[1] + "m_" + str(i))
-  fields[0].set_homogenuous_field(state, 0.0, 0.0, fastenup * state.t/50e-9/Constants.mu0)
+  fields[0].set_homogenuous_field(0.0, 0.0, fastenup * state.t/50e-9/Constants.mu0)
   Llg.step(state)
-  printzee = af.mean(af.mean(af.mean(fields[0].get_zee(), dim=0), dim=1), dim=2)
+  printzee = af.mean(af.mean(af.mean(fields[0].h(state), dim=0), dim=1), dim=2)
   print(state.t, state.m_mean(0), state.m_mean(1), state.m_mean(2), fastenup * state.t/50e-9/Constants.mu0, printzee[0,0,0,2].scalar()*Constants.mu0)
   stream.write("%e, %e, %e, %e, %e, %e\n" %(state.t, state.m_mean(0), state.m_mean(1), state.m_mean(2), fastenup * state.t/50e-9/Constants.mu0, printzee[0,0,0,2].scalar()))
   stream.flush()
