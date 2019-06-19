0bbff3e4f67578015b1ce94f7cface1321fe2763
paul <paul.thomas.heistracher@univie.ac.at>
2019-06-19 16:32:30 +0200

    domainwall_pinning_x.py

--- scripts/domainwall_pinning/domainwall_pinning_x.py
+++ scripts/domainwall_pinning/domainwall_pinning_x.py
@@ -5,6 +5,8 @@ from math import sqrt
 import sys
 import time
 
+print(0.25/Constants.mu0)
+print(1/Constants.mu0)
 if len(sys.argv) > 2:
     af.set_device(int(sys.argv[2]))
 af.info()
@@ -37,37 +39,35 @@ def H(soft_Aex, soft_ms, soft_K_uni, hard_Aex, hard_ms, hard_K_uni):
     hard_J=hard_ms*Constants.mu0
     return 2*hard_K_uni/hard_J * ((1-eps_k*eps_A)/(1+sqrt(eps_ms*eps_A))**2)
 
-print ("H_analytic=", H(soft_Aex, soft_ms, soft_K_uni, hard_Aex, hard_ms, hard_K_uni)*Constants.mu0, " [T]")
-
 # Creating objects
 m = af.constant(0.0, nx, ny, nz, 3, dtype=af.Dtype.f64)
-m[:nx/2, :, :, 0] = af.constant( 1.0, int(nx/2) , ny, nz, 1, dtype=af.Dtype.f64)
-m[:nx/2, :, :, 1] = af.constant( 0.3, int(nx/2) , ny, nz, 1, dtype=af.Dtype.f64)
-m[nx/2:, :, :, 0] = af.constant(-1.0, int(nx/2) , ny, nz, 1, dtype=af.Dtype.f64)
-m[nx/2:, :, :, 1] = af.constant( 0.3, int(nx/2) , ny, nz, 1, dtype=af.Dtype.f64)
+m[:nx/2, :, :, 0] =  1.0
+m[:nx/2, :, :, 1] =  0.3
+m[nx/2:, :, :, 0] = -1.0
+m[nx/2:, :, :, 1] =  0.3
 
 mesh = Mesh(nx, ny, nz, x/nx, y/ny, z/nz)
 
 # Setting A values as field
 A_field = af.constant(0.0, nx, ny, nz, 3, dtype=af.Dtype.f64)
 # soft
-A_field[:nx/2, :, :, :] = af.constant(soft_Aex, int(nx/2) , ny, nz, 3, dtype=af.Dtype.f64)
+A_field[:nx/2, :, :, :] = soft_Aex
 # hard
-A_field[nx/2:, :, :, :] = af.constant(hard_Aex, int(nx/2) , ny, nz, 3, dtype=af.Dtype.f64)
+A_field[nx/2:, :, :, :] = hard_Aex
 
 # Setting Ms values as field
 Ms_field = af.constant(0.0, nx, ny, nz, 3, dtype=af.Dtype.f64)
 # soft
-Ms_field[:nx/2, :, :, :] = af.constant(soft_ms, int(nx/2) , ny, nz, 3, dtype=af.Dtype.f64)
+Ms_field[:nx/2, :, :, :] = soft_ms
 # hard
-Ms_field[nx/2:, :, :, :] = af.constant(hard_ms, int(nx/2) , ny, nz, 3, dtype=af.Dtype.f64)
+Ms_field[nx/2:, :, :, :] = hard_ms
 
 # Setting Ku1 values as field
 Ku1_field = af.constant(0.0, nx, ny, nz, 3, dtype=af.Dtype.f64)
 # soft
-Ku1_field[:nx/2, :, :, :] = af.constant(soft_K_uni, int(nx/2) , ny, nz, 3, dtype=af.Dtype.f64)
+Ku1_field[:nx/2, :, :, :] = soft_K_uni
 # hard
-Ku1_field[nx/2:, :, :, :] = af.constant(hard_K_uni, int(nx/2) , ny, nz, 3, dtype=af.Dtype.f64)
+Ku1_field[nx/2:, :, :, :] = hard_K_uni
 
 state = State(mesh, Ms_field, m)
 state.normalize()
@@ -76,28 +76,34 @@ fields = [
     ExternalField(af.constant(0.0, nx, ny, nz, 3, dtype=af.Dtype.f64)),
     ExchangeField(A_field),
     UniaxialAnisotropyField(Ku1_field, Ku1_axis=[1., 0., 0.]),
+    #DemagField(mesh),
 ]
 Llg = LLGIntegrator(alpha=1.0, terms=fields)
 
-fastenup = 10
+maxField = 2./Constants.mu0 # 2 [T]
+simtime = 10e-9 # [s]
+#simtime = 3.1e-10 # [s] # yields correct result by coincidence
+
 print("Start 100 [ns] hysteresis")
 stream = open(sys.argv[1]+"m.dat", "w")
 timer = time.time()
 i = 0
-while (state.t < 1e-7/fastenup and state.m_mean(0) < (1. - 1e-6)):
-  if i%2000 == 0:
-    state.write_vti(sys.argv[1] + "m_" + str(i))
-  fields[0].set_homogeneous_field(fastenup * state.t/50e-9/Constants.mu0, 0.0, 0.0)
+nvti = 0
+while (state.t < simtime and state.m_mean(0) < (1. - 1e-6)):
+  if i%300 == 0:
+    state.write_vti(sys.argv[1] + "m_" + str(nvti))
+    nvti = nvti + 1
+  fields[0].set_homogeneous_field(state.t / simtime * maxField, 0.0, 0.0)
   Llg.step(state)
-  #mean = af.mean(af.mean(af.mean(state.m, dim=0), dim=1), dim=2)
   printzee = af.mean(af.mean(af.mean(fields[0].h(state), dim=0), dim=1), dim=2)
-  print(state.t, state.m_mean(0), state.m_mean(1), state.m_mean(2), state.t/50e-9/Constants.mu0, printzee[0, 0, 0, 0].scalar()*Constants.mu0)
-  #printzee = af.mean(af.mean(af.mean(fields[0].get_zee(), dim=0), dim=1), dim=2)
+  print(state.t/simtime, state.m_mean(0), state.t /simtime * maxField, printzee[0, 0, 0, 0].scalar()*Constants.mu0)
   stream.write("%e, %e, %e, %e, %e, %e\n" %(state.t, state.m_mean(0), state.m_mean(1), state.m_mean(2), state.t/50e-9/Constants.mu0, printzee[0, 0, 0, 0].scalar()))
   stream.flush()
   i = i + 1
-print("100 [ns] hysteresis in ", time.time() - timer, "[s]")
 
 stream.close()
+state.write_vti(sys.argv[1] + "m_switched")
+print("simulated", state.t, "[s] (out of", simtime, ") in", time.time() - timer, "[s]")
+print("total time =", time.time() - start, "[s]\n")
+print("H_analytic=", H(soft_Aex, soft_ms, soft_K_uni, hard_Aex, hard_ms, hard_K_uni)*Constants.mu0, " [T]")
 print("switched at Hext=", printzee[0, 0, 0, 0].scalar()*Constants.mu0, " [T] with state.t=", state.t, " [s]")
-print("total time =", time.time() - start, "[s]")
--- scripts/domainwall_pinning/run_x.sh
+++ scripts/domainwall_pinning/run_x.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}")"
-../magnum.af -fs domainwall_pinning_x.py "$HOME"/data/domanwall_pinning/x/
+../magnum.af -fs -p plot_x.sh domainwall_pinning_x.py "$HOME"/data/domanwall_pinning/x/
