5541c070a144e0682fed1d864dcb02c2a8fe4eba
paul <paul.thomas.heistracher@univie.ac.at>
2019-06-24 11:52:58 +0200

    integrators/new_llg.cpp \n in printf() and cleanup

--- src/integrators/new_llg.cpp
+++ src/integrators/new_llg.cpp
@@ -48,19 +48,12 @@ void LLGIntegrator::relax(State& state, const double precision, const int iloop,
         for ( int i = 0; i<iloop; i++){
             step(state);
         }
-        if( iwritecout > 0 and state.steps % iwritecout == 0) printf("LLGIntegrator: Relax: step %llu, rdiff= %e", state.steps, fabs((E_prev - E(state))/E_prev));
+        if( iwritecout > 0 and state.steps % iwritecout == 0) printf("LLGIntegrator: Relax: step %llu, rdiff= %e\n", state.steps, fabs((E_prev - E(state))/E_prev));
     }
-    printf("timerelax [af-s]: %e . Current state.steps= %llu and state.t = %e", state.t, state.steps, af::timer::stop(t));
+    printf("timerelax [af-s]: %e . Current state.steps= %llu and state.t = %e\n", state.t, state.steps, af::timer::stop(t));
 }
 
 long int LLGIntegrator::h_addr(const State& state){
-    //std::vector<af::array> h_addr_temp_array;
-    //h_addr_temp_array.push_back(fheff(state));
-    //h_addr_temp_array.back().lock();
-    //return (long int) h_addr_temp_array.back().get();
-
-    //TODO tempfix for wrapping, elaborate other solution
     af::array *heff = new af::array(fheff(state));
     return (long int) heff->get();
-    //return (long int) fheff(state).get();
 }
