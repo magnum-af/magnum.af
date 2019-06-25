5dfdedcd27493ad2c08354fb1ff635c068c8192f
paul <paul.thomas.heistracher@univie.ac.at>
2019-06-25 15:37:06 +0200

    tests/zero_crossing.cpp adding two cases

--- tests/unit/cpp/zero_crossing.cpp
+++ tests/unit/cpp/zero_crossing.cpp
@@ -33,6 +33,26 @@ TEST(ZeroCrossing, x_plus_1) {
 }
 
 
+TEST(ZeroCrossing, x_plus_1_negative_range) {
+    double precision = 1e-12;
+    ZeroCrossing zc(x_plus_1, precision, 10, 2.1, -2, 100, false);
+    auto result = zc.calc_x_and_f();
+    EXPECT_NEAR(-1, result.first, precision);
+    EXPECT_NEAR(0, result.second, precision);
+}
+
+
+double minus_x_plus_1(double x){return -(x + 1);}
+
+TEST(ZeroCrossing, minus_x_plus_1) {
+    double precision = 1e-12;
+    ZeroCrossing zc(minus_x_plus_1, precision, 10, -2.1, 2, 100, false);
+    auto result = zc.calc_x_and_f();
+    EXPECT_NEAR(-1, result.first, precision);
+    EXPECT_NEAR(0, result.second, precision);
+}
+
+
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
