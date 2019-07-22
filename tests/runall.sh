dc5c337041a772f64e140861f6157095ee8465a1
paul <paul.thomas.heistracher@univie.ac.at>
2019-07-22 12:12:12 +0200

    tests adding cpu opencl cuda info in runnall script

--- tests/runall.sh
+++ tests/runall.sh
@@ -3,7 +3,9 @@
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # call this scripts directory
 
 for testbin in ../bin/test*; do
+    echo "running test '$testbin':"
     ./$testbin
+    echo ""
 done
 ./unit/run_all_unit_tests.sh
 ./integration/run_all_integration_tests.sh
