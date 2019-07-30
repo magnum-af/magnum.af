1b1aba2ec29452351e205a03f8a412657c354244
Paul Heistracher <e1225725@student.tuwien.ac.at>
2019-07-30 21:57:42 +0200

    named_type passing by ref

--- src/util/named_type.hpp
+++ src/util/named_type.hpp
@@ -1,12 +1,18 @@
 #pragma once
 #include<utility>
 
+//from: https://www.fluentcpp.com/2017/03/06/passing-strong-types-reference-revisited/
 template <typename T, typename Parameter>
 class NamedType
 {
 public:
     explicit NamedType(T const& value) : value_(value) {}
-    explicit NamedType(T&& value) : value_(std::move(value)) {}
+    template<typename T_ = T>
+    explicit NamedType(T&& value,
+        typename std::enable_if<!std::is_reference<T_>{},
+        std::nullptr_t>::type = nullptr)
+    : value_(std::move(value)) {}
+
     T& get() { return value_; }
     T const& get() const {return value_; }
 private:
