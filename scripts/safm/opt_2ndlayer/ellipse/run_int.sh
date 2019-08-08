4c8682d5733899881e5120b20c32e64dae3b4926
paul <paul.thomas.heistracher@univie.ac.at>
2019-08-08 15:22:11 +0200

    sim script update

--- scripts/safm/opt_2ndlayer/ellipse/run_int.sh
+++ scripts/safm/opt_2ndlayer/ellipse/run_int.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-GPU="2"
+GPU="0"
 
 # geometry
-nx="128"
-ny="32"
 x="100e-9"
 y="25e-9"
+nx="128"
+ny="32"
 
 # hys info
 hzee_max="2.00" # maximum H_external for hysteresis loop
 integr_time="10.0e-9"
 
 # write dirs
-rootdir="$HOME"/data_magnum.af/safm/opt_2ndlayer/ellipse_"$nx"nx_"$ny"ny_"$x"x_"$y"y/
+rootdir="$HOME"/data_magnum.af/safm/opt_2ndlayer/ellipse_"$x"x_"$y"y_"$nx"nx_"$ny"ny/
 writedir_hf="$rootdir"h_free_layer/
 writedir_hys="$rootdir"rk_"$hzee_max"T_"$integr_time"s/
 
@@ -24,5 +24,4 @@ if [ ! -f "$writedir_hf/h_free_layer.vti" ]; then
 fi
 
 # calculating hysteresis
-
 ../../../magnum.af -g "$GPU" -p plot.gpi safm_ellipse_hys_rk.cpp "$writedir_hys" "$hzee_max" "$integr_time" "$writedir_hf"/h_free_layer.vti
--- scripts/safm/opt_2ndlayer/ellipse/run_min.sh
+++ scripts/safm/opt_2ndlayer/ellipse/run_min.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-GPU="1"
+GPU="0"
 
 # geometry
-nx="128"
-ny="32"
 x="100e-9"
 y="25e-9"
+nx="128"
+ny="32"
 
 # hys info
 hzee_max="2.0" # maximum H_external for hysteresis loop
 steps="200"
 
 # write dirs
-rootdir="$HOME"/data_magnum.af/safm/opt_2ndlayer/ellipse_"$nx"nx_"$ny"ny_"$x"x_"$y"y/
+rootdir="$HOME"/data_magnum.af/safm/opt_2ndlayer/ellipse_"$x"x_"$y"y_"$nx"nx_"$ny"ny/
 writedir_hf="$rootdir"h_free_layer/
 writedir_hys="$rootdir"min_"$hzee_max"T_"$steps"steps/
 
@@ -25,5 +25,4 @@ fi
 
 
 # calculating hysteresis
-
 ../../../magnum.af -g "$GPU" -p plot.gpi safm_ellipse_hys_minimizer.cpp "$writedir_hys" "$hzee_max" "$steps" "$writedir_hf"/h_free_layer.vti
