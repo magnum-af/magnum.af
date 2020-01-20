48acbe36aa2d2d1d39b2c183dc20a2465150b5f8
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2020-01-20 15:49:29 +0100

    cmake adding git_watcher and integrating in header, adding magnumafinfo

---
+++ examples/magnumafcpp_info.cpp
@@ -0,0 +1,27 @@
+#include "magnum_af.hpp"
+#include "magnumafConfig.hpp"
+#include "magnumafConfig_git.hpp"
+#include "arrayfire.h"
+
+using namespace magnumafcpp;
+
+int main()
+{
+    af::info();
+
+    std::cout << "magnumafcpp_VERSION_MAJOR = " << magnumafcpp_VERSION_MAJOR << std::endl;
+    std::cout << "magnumafcpp_VERSION_MINOR = " << magnumafcpp_VERSION_MINOR << std::endl;
+    std::cout << "magnumafcpp_VERSION_PATCH = " << magnumafcpp_VERSION_PATCH << std::endl;
+    std::cout << "magnumafcpp_VERSION_TWEAK = " << magnumafcpp_VERSION_TWEAK << std::endl;
+    std::cout << "magnumafcpp_VERSION       = " << magnumafcpp_VERSION       << std::endl;
+
+    std::cout << "GIT_RETRIEVED_STATE       = " << GIT_RETRIEVED_STATE       << std::endl;
+    std::cout << "GIT_HEAD_SHA1             = " << GIT_HEAD_SHA1             << std::endl;
+    std::cout << "GIT_IS_DIRTY              = " << GIT_IS_DIRTY              << std::endl;
+    std::cout << "GIT_AUTHOR_NAME           = " << GIT_AUTHOR_NAME           << std::endl;
+    std::cout << "GIT_AUTHOR_EMAIL          = " << GIT_AUTHOR_EMAIL          << std::endl;
+    std::cout << "GIT_COMMIT_DATE_ISO8601   = " << GIT_COMMIT_DATE_ISO8601   << std::endl;
+    std::cout << "GIT_COMMIT_SUBJECT        = " << GIT_COMMIT_SUBJECT        << std::endl;
+    std::cout << "GIT_COMMIT_BODY           = " << GIT_COMMIT_BODY           << std::endl;
+    return 0;
+}
--- scripts/single_script_build/main_empty.cpp
+++ scripts/single_script_build/main_empty.cpp
@@ -1,14 +1,8 @@
 #include "magnum_af.hpp"
-#include "magnumafConfig.hpp"
 
 using namespace magnumafcpp;
 
 int main()
 {
-    std::cout << "magnumafcpp_VERSION_MAJOR = " << magnumafcpp_VERSION_MAJOR << std::endl;
-    std::cout << "magnumafcpp_VERSION_MINOR = " << magnumafcpp_VERSION_MINOR << std::endl;
-    std::cout << "magnumafcpp_VERSION_PATCH = " << magnumafcpp_VERSION_PATCH << std::endl;
-    std::cout << "magnumafcpp_VERSION_TWEAK = " << magnumafcpp_VERSION_TWEAK << std::endl;
-    std::cout << "magnumafcpp_VERSION       = " << magnumafcpp_VERSION       << std::endl;
     return 0;
 }
