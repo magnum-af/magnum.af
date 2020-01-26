b19cbe707ecb327fbb4bb2bef90e8a31280b49e9
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2020-01-26 21:33:21 +0100

    boost: wrapping Mesh class

--- boostpython/src/bindings.cpp
+++ boostpython/src/bindings.cpp
@@ -1,10 +1,23 @@
 #include <boost/python.hpp>
 #include "func.hpp"
+#include "mesh.hpp"
 
+using namespace boost::python;
+using namespace magnumafcpp;
 
 BOOST_PYTHON_MODULE(PYBINDTESTLIB)
 {
     Py_Initialize();
-    using namespace boost::python;
     def("greet", greet);
+
+    class_<Mesh>("Mesh", init<uint32_t, uint32_t, uint32_t, double, double, double>())
+        .def(init<>())
+        .def_readwrite("V", &Mesh::V)
+        .def_readwrite("nx", &Mesh::n0)
+        .def_readwrite("ny", &Mesh::n1)
+        .def_readwrite("nz", &Mesh::n2)
+        .def_readwrite("dx", &Mesh::dx)
+        .def_readwrite("dy", &Mesh::dy)
+        .def_readwrite("dz", &Mesh::dz)
+    ;
 }
--- boostpython/src/greet.py
+++ boostpython/src/greet.py
@@ -2,3 +2,8 @@
 
 from PYBINDTESTLIB import greet
 print(greet())
+
+
+from PYBINDTESTLIB import Mesh
+mesh = Mesh(1, 2, 3, 0.1, 0.2, 0.3)
+print(mesh.nx, mesh.ny, mesh.nz, mesh.dx, mesh.dy, mesh.dz, mesh.V)
