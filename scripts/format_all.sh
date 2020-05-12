c14033530575e6c23926e2b3356deea39cff659d
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2020-05-12 19:15:32 +0200

    apply cmake-format

---
+++ scripts/format_all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# assumes dir ../ is project root dir
+
+# call this scripts directory
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# cd into project root dir
+cd ../
+
+echo "Running formatting from dir $PWD"
+
+set -o xtrace
+
+# format C++
+find . -iname '*.hpp' -o -iname '*.cpp' | xargs clang-format -i --style=file
+
+# format CMake
+find . -iname 'CMakeLists.txt' | xargs cmake-format -i
