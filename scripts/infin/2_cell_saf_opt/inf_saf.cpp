186f10b1c7f74da4907a190370bf2b3360afbc33
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2020-06-23 13:47:21 +0200

    update inf_saf.cpp

--- scripts/infin/2_cell_saf_opt/inf_saf.cpp
+++ scripts/infin/2_cell_saf_opt/inf_saf.cpp
@@ -1,6 +1,6 @@
 #include "arrayfire.h"
 #include "magnum_af.hpp"
-#include <cmath>
+#include <numeric>
 
 using namespace magnumafcpp;
 
@@ -14,12 +14,15 @@ int main(int argc, char** argv) {
     af::info();
 
     // Parameter initialization
+    // z1: Pinned Layer
+    // z2: Reference Layer
     const int nx = 1, ny = 1, nz = 2;
-    const double dx = 10e-9;
+    const double dx = 1.5e-9;
+    // const double dx = 10e-9;
+    auto _1D_field = af::dim4(nx, ny, nz, 1);
 
-    // const double Ms = 0.37 / constants::mu0;
-    const double Ms1 = 1.4e6; // bottom
-    const double Ms2 = 1e6;   // top
+    const double Ms1 = 1.4e6;
+    const double Ms2 = 1e6;
     const double RKKY = -0.8e-3 * dx;
     const double A = 15e-12; // Note: A is replaced by RKKY here
     // antoferomagnetic coupling simulated by external field
@@ -31,12 +34,19 @@ int main(int argc, char** argv) {
     std::cout << "H_af[Oe]=" << H_af << std::endl;
     std::cout << "H_af [T]=" << H_af * constants::mu0 << std::endl;
 
-    // const double K1 = 0.2e6;
-    // const double K2 = 0.2e6;
+    //// Anisotropy
+    //// const double K1 = 0.2e6;
+    //// const double K2 = 0.2e6;
+    // const double K1 = -1 / 2. * std::pow(Ms1, 2) * constants::mu0;
+    // const double K2 = -1 / 2. * std::pow(Ms2, 2) * constants::mu0;
+    // std::cout << "K1=" << K1 << std::endl;
+    // std::cout << "K2=" << K2 << std::endl;
     // const std::array<double, 3> Ku1_axis = {0, 0, 1}; // TODO
+    // af::array Ku1_field = af::constant(0.0, _1D_field, f64);
     // Ku1_field(af::span, af::span, 0) = K1;
     // Ku1_field(af::span, af::span, 1) = K2;
     // auto aniso = LlgTerm(new UniaxialAnisotropyField(Ku1_field, Ku1_axis));
+
     // Generating Objects
     Mesh mesh(nx, ny, nz, dx, dx, dx);
 
@@ -44,21 +54,15 @@ int main(int argc, char** argv) {
     af::array m = af::constant(0.0, mesh.dims, f64);
     m(af::span, af::span, 0, 0) = 1.;
     m(af::span, af::span, 1, 0) = -1.;
-    auto _1D_field = af::dim4(nx, ny, nz, 1);
     af::array Ms_field = af::constant(0.0, _1D_field, f64);
     Ms_field(af::span, af::span, 0) = Ms1;
     Ms_field(af::span, af::span, 1) = Ms2;
-    af::array Ku1_field = af::constant(0.0, _1D_field, f64);
     State state(mesh, Ms_field, m);
     state.write_vti(filepath + "minit");
 
     auto rkky = LlgTerm(new RKKYExchangeField(
         RKKY_values(af::constant(RKKY, mesh.dims, f64)),
         Exchange_values(af::constant(A, mesh.dims, f64)), mesh));
-
-    // auto ilex = LlgTerm(new RKKYExchangeField(
-    //    RKKY_values(af::constant(ILex, mesh.dims, f64)),
-    //    Exchange_values(af::constant(A, mesh.dims, f64)), mesh));
 
     auto demag = LlgTerm(new DemagField(mesh, true, true, 0));
 
@@ -79,51 +83,39 @@ int main(int argc, char** argv) {
         return zee;
     };
     auto external = LlgTerm(new ExternalField(zee_func));
-    // LLGIntegrator llg(1, {demag, rkky, external, aniso, ilex});
     LLGIntegrator llg(1, {demag, rkky, external});
+    // LLGIntegrator llg(1, {demag, rkky, external, aniso});
 
     std::ofstream stream(filepath + "m.dat");
     stream.precision(12);
 
-    std::vector<double> vecE;
+    std::vector<double> abs_my_rl; // Ref Layer my list
 
-    // for (unsigned i = 0; i < steps_full_hysteresis; i++) {
     // for (unsigned i = 0; i < 360; i++) {
-    for (unsigned i = 0; i < 360; i += 20) {
+    for (unsigned i = 0; i <= 360; i += 20) {
         current_step = i;
-        // for (unsigned i = 0; i < steps_full_hysteresis; i++) {
-        // llg.step(state);
         llg.relax(state);
-        std::cout << i << "\t"
-                  << llg.llgterms[2]->h(state)(0, 0, 0, 0).scalar<double>() *
-                         constants::mu0
-                  << "\t" << state.m(0, 0, 0, 1).scalar<double>() << "\t"
-                  << state.m(0, 0, 1, 1).scalar<double>() << std::endl;
-        stream << i << "\t"
-               << llg.llgterms[2]->h(state)(0, 0, 0, 1).scalar<double>() *
-                      constants::mu0
-               << "\t" << state.m(0, 0, 0, 0).scalar<double>() << "\t"
-               << state.m(0, 0, 1, 1).scalar<double>() << std::endl;
+        const double Hx_component =
+            llg.llgterms[2]->h(state)(0, 0, 1, 0).scalar<double>() *
+            constants::mu0;
+        const double my_z0 = state.m(0, 0, 0, 1).scalar<double>();
+        const double my_z1 = state.m(0, 0, 1, 1).scalar<double>();
+        abs_my_rl.push_back(std::abs(my_z1));
+
+        std::cout << i << "\t" << Hx_component << "\t" << my_z0 << "\t" << my_z1
+                  << std::endl;
+        stream << i << "\t" << Hx_component << "\t" << my_z0 << "\t" << my_z1
+               << std::endl;
     }
     stream.close();
 
+    for (auto it : abs_my_rl) {
+        std::cout << it << std::endl;
+    }
+    double sum = std::accumulate(abs_my_rl.begin(), abs_my_rl.end(), 0.0);
+    double mean = sum / abs_my_rl.size();
+    std::cout << "sum=" << sum << std::endl;
+    std::cout << "mean=" << mean << std::endl;
+
     return 0;
 }
-// double field_Tesla;
-// if (current_step < steps_full_hysteresis / 4) {
-//    field_Tesla = hzee_max * 4. * current_step /
-//    steps_full_hysteresis;
-//} else if (current_step < 3 * steps_full_hysteresis / 4) {
-//    field_Tesla =
-//        -hzee_max * 4. * current_step / steps_full_hysteresis +
-//        2 * hzee_max;
-//} else if (current_step < steps_full_hysteresis) {
-//    field_Tesla = hzee_max * 4. * current_step / steps_full_hysteresis
-//    -
-//                  4 * hzee_max;
-//} else {
-//    field_Tesla = 0;
-//    std::cout << "WARNING ZEE time out of range, setting external "
-//                 "field to zero"
-//              << std::endl;
-//}
--- scripts/infin/2_cell_saf_opt/run_inf_saf.sh
+++ scripts/infin/2_cell_saf_opt/run_inf_saf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-../../magnum.af -p plot_inf_saf.gpi -f inf_saf.cpp testrun/
+../../magnum.af -p plot_inf_saf.gpi -f inf_saf.cpp $HOME/data_magnum.af/inf_saf/testrun/
 
 # prev in examples dir:
 #(cd ../build/ && make -j) && cd ../bin && ./inf_saf_cpu run_inf_saf/ && cat run_inf_saf/m.dat
