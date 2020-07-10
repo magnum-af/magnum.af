0f6b44ef4eceed2284bed84fc4041b25f42a8d03
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2020-07-10 15:05:59 +0200

    disable empy uint test

--- test/unit/nonequi_external_field.cpp
+++ test/unit/nonequi_external_field.cpp
@@ -6,25 +6,25 @@
 
 using namespace magnumafcpp;
 
-TEST(NonequiExternalField, E) {
-    int nx = 1;
-    int ny = 2;
-    double dx = 1;
-    double dy = 2;
-    std::vector<double> dz = {1, 2, 3};
-    NonequispacedMesh nemesh(nx, ny, dx, dy, dz);
-    af::array m = af::constant(1., nx, ny, nemesh.nz, 3, f64);
-    af::array field = af::constant(1., nx, ny, nemesh.nz, 3, f64);
-    NonequiExternalField nonequi_external(nemesh, field);
-    ExternalField external(field);
-    double Ms = 1;
-    State state(m, Ms);
-    // TODO:
-    // EXPECT_EQ(nonequi_external.E(state_nemesh), external.E(state_nemesh));
-    // State state_mesh(Mesh(nx, ny, nemesh.nz, dx, dy, dz[0]), 1, m);
-    // EXPECT_EQ(nonequi_external.E(state_nemesh), external.E(state_mesh));
-    // EXPECT_EQ(nonequi_external.E(state_nemesh), 0);
-}
+// TEST(NonequiExternalField, E) {
+//    int nx = 1;
+//    int ny = 2;
+//    double dx = 1;
+//    double dy = 2;
+//    std::vector<double> dz = {1, 2, 3};
+//    NonequispacedMesh nemesh(nx, ny, dx, dy, dz);
+//    af::array m = af::constant(1., nx, ny, nemesh.nz, 3, f64);
+//    af::array field = af::constant(1., nx, ny, nemesh.nz, 3, f64);
+//    NonequiExternalField nonequi_external(nemesh, field);
+//    ExternalField external(field);
+//    double Ms = 1;
+//    State state(m, Ms);
+//    // TODO:
+//    // EXPECT_EQ(nonequi_external.E(state_nemesh), external.E(state_nemesh));
+//    // State state_mesh(Mesh(nx, ny, nemesh.nz, dx, dy, dz[0]), 1, m);
+//    // EXPECT_EQ(nonequi_external.E(state_nemesh), external.E(state_mesh));
+//    // EXPECT_EQ(nonequi_external.E(state_nemesh), 0);
+//}
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
