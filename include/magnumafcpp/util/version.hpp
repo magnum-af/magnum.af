94b0d6ee6137317015ee2f25c5395244e7b66419
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2020-07-15 08:37:38 +0200

    avoiding gnu confict with <major>

--- include/magnumafcpp/util/version.hpp
+++ include/magnumafcpp/util/version.hpp
@@ -5,18 +5,18 @@
 
 ///
 /// Struct for version comparison.
-/// Using: major.minor.revision.build, e.g: 3.7.2.0
+/// Using: major_.minor.revision.build, e.g: 3.7.2.0
 ///
 struct Version {
-    int major = 0, minor = 0, revision = 0, build = 0;
+    int major_ = 0, minor = 0, revision = 0, build = 0;
 
-    Version(std::string version) { std::sscanf(version.c_str(), "%d.%d.%d.%d", &major, &minor, &revision, &build); }
+    Version(std::string version) { std::sscanf(version.c_str(), "%d.%d.%d.%d", &major_, &minor, &revision, &build); }
 
-    Version(int major, int minor = 0, int revision = 0, int build = 0)
-        : major(major), minor(minor), revision(revision), build(build) {}
+    Version(int major_, int minor = 0, int revision = 0, int build = 0)
+        : major_(major_), minor(minor), revision(revision), build(build) {}
 
     bool operator<(const Version& other) {
-        if (major < other.major) {
+        if (major_ < other.major_) {
             return true;
         } else if (minor < other.minor) {
             return true;
@@ -30,7 +30,7 @@ struct Version {
     }
 
     bool operator>(const Version& other) {
-        if (major > other.major) {
+        if (major_ > other.major_) {
             return true;
         } else if (minor > other.minor) {
             return true;
@@ -44,7 +44,7 @@ struct Version {
     }
 
     bool operator>=(const Version& other) {
-        if (major > other.major) {
+        if (major_ > other.major_) {
             return true;
         } else if (minor > other.minor) {
             return true;
@@ -53,16 +53,16 @@ struct Version {
         } else if (build > other.build) {
             return true;
         } else {
-            return major == other.major && minor == other.minor && revision == other.revision && build == other.build;
+            return major_ == other.major_ && minor == other.minor && revision == other.revision && build == other.build;
         }
     }
 
     bool operator==(const Version& other) {
-        return major == other.major && minor == other.minor && revision == other.revision && build == other.build;
+        return major_ == other.major_ && minor == other.minor && revision == other.revision && build == other.build;
     }
 
     std::ostream& operator<<(std::ostream& stream) {
-        stream << this->major;
+        stream << this->major_;
         stream << '.';
         stream << this->minor;
         stream << '.';
@@ -73,13 +73,13 @@ struct Version {
     }
 };
 
-/// Returning arrayfire major, minor, patch version
+/// Returning arrayfire major_, minor, patch version
 std::array<int, 3> af_version() {
     const std::string s = af::infoString();
-    int major = std::stoi(s.substr(11, 1));
+    int major_ = std::stoi(s.substr(11, 1));
     int minor = std::stoi(s.substr(13, 1));
     int patch = std::stoi(s.substr(15, 1));
-    return {major, minor, patch};
+    return {major_, minor, patch};
 }
 
 /// Returning arrayfire version string
