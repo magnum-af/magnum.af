273fb69272393a39b4cb2f2ccdbac33cfa1dfabd
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2020-07-21 12:07:58 +0200

    update convert_vti_to_ascii.cpp

--- examples/convert_vti_to_ascii.cpp
+++ examples/convert_vti_to_ascii.cpp
@@ -1,3 +1,5 @@
+// usage: ./convert_vti_to_ascii /path/to/vtifile.vti /path/to/newascii.txt
+
 #include "arrayfire.h"
 #include "magnum_af.hpp"
 #include <string>
@@ -7,22 +9,19 @@ using namespace magnumafcpp;
 int main(int argc, char** argv) {
     // Checking input variables and setting GPU Device
     for (int i = 0; i < argc; i++) {
-        std::cout << "Parameter " << i << " was " << argv[i] << std::endl;
+        std::cout << "argv[" << i << "]: " << argv[i] << std::endl;
     }
-    std::string filepath(argc > 1 ? argv[1] : "output_magnum.af/");
-    af::setDevice(argc > 2 ? std::stoi(argv[2]) : 0);
-    af::info();
 
     Mesh mesh(0, 0, 0, 0, 0, 0);
     af::array m;
 
-    vti_reader(m, mesh, filepath + argv[3]);
+    vti_reader(m, mesh, argv[1]);
     // vti_writer_micro(m(af::span, af::span, 0, af::span), Mesh(mesh.n0,
     // mesh.n1, 1, mesh.dx, mesh.dy, mesh.dz), filepath + "toplayer_" +
     // argv[3]); write_ascii(m(af::span, af::span, 0, af::span), Mesh(mesh.n0,
     // mesh.n1, 1, mesh.dx, mesh.dy, mesh.dz), filepath + "toplayer_" +
     // argv[4]);
-    write_ascii(m, mesh, filepath + argv[4]);
+    write_ascii(m, mesh, argv[2]);
     // read_ascii(m, mesh, filepath + argv[4]);
 
     return 0;
