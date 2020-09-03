158981b19c4a17cb2010985aeaf18aa76e749e1d
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2020-09-03 11:34:17 +0200

    test/unit/state.cpp

--- test/unit/state.cpp
+++ test/unit/state.cpp
@@ -1,5 +1,7 @@
 #include "state.hpp"
 #include "func.hpp"
+#include <gmock/gmock-matchers.h>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <vector>
 
@@ -31,6 +33,42 @@ TEST(State, normalize_m) {
     EXPECT_EQ(state.m(0, 0, 0, 0).scalar<double>(), 1 / std::sqrt(3));
     EXPECT_EQ(state.m(0, 0, 0, 1).scalar<double>(), 1 / std::sqrt(3));
     EXPECT_EQ(state.m(0, 0, 0, 2).scalar<double>(), 1 / std::sqrt(3));
+}
+
+// Testing State::mean_m() for scalar and array (zeros/nozeros) Ms
+TEST(State, mean_m) {
+    Mesh mesh(2, 2, 2, 1e-9, 1e-9, 1e-9);
+    double Ms = 1e6;
+
+    auto test = [](State state, std::array<double, 3> res) { EXPECT_THAT(state.mean_m(), res); };
+
+    // vec_as_array method:
+    auto m = [dims = mesh.dims_scalar](std::array<double, 3> vec) {
+        return af::tile(af::array(af::dim4(1, 1, 1, 3), vec.data()), dims);
+    };
+
+    // scalar Ms
+    test({mesh, Ms, m({1, 0, 0})}, {1, 0, 0});
+    test({mesh, Ms, m({0, 1, 0})}, {0, 1, 0});
+    test({mesh, Ms, m({0, 0, 1})}, {0, 0, 1});
+
+    // Ms.field
+    auto Ms_field = af::constant(Ms, mesh.dims_scalar, f64);
+    test({mesh, Ms_field, m({1, 0, 0})}, {1, 0, 0});
+    test({mesh, Ms_field, m({0, 1, 0})}, {0, 1, 0});
+    test({mesh, Ms_field, m({0, 0, 1})}, {0, 0, 1});
+
+    // Ms.field with zeros
+    auto Ms_field_with_zeros = af::constant(0, mesh.dims_scalar, f64);
+    Ms_field_with_zeros(0, 0, 0) = Ms;
+    test({mesh, Ms_field_with_zeros, m({1, 0, 0})}, {1, 0, 0});
+    test({mesh, Ms_field_with_zeros, m({0, 1, 0})}, {0, 1, 0});
+    test({mesh, Ms_field_with_zeros, m({0, 0, 1})}, {0, 0, 1});
+
+    // This should work:
+    // auto res = State(mesh, Ms_field_with_zeros, m({0, 0, 1})).mean_m();
+    // EXPECT_THAT(res, {0, 0, 1});
+    // EXPECT_THAT(res, std::array<double, 3>({0, 0, 1}));
 }
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
