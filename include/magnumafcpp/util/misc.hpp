dfbaf0db2acc5224c1ba07fb44778c01371d190a
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2020-11-23 15:57:29 +0100

    remove unix-dependent cache cleanup methods, now done via CacheManager

--- include/magnumafcpp/util/misc.hpp
+++ include/magnumafcpp/util/misc.hpp
@@ -4,19 +4,6 @@
 
 namespace magnumafcpp {
 
-unsigned long long GetDirSize(std::string filepath);
-///> Returns true if file "name" exists, false otherwise
-inline bool exists(const std::string& absolute_filepath) {
-    struct stat buffer;
-    return (stat(absolute_filepath.c_str(), &buffer) == 0);
-}
-
-inline bool createdir(const std::string& absolute_filepath);
-std::string setup_magafdir();
-
-// void directoryManager(char *dir, int maxNumberOfFiles);
-void remove_oldest_files_until_size(const char* dir, unsigned long long maxNumberOfBytes, bool verbose = true);
-
 ///> Colorizing a std::string for std::cout. Red is for warnings.
 inline std::string red(const std::string str) { return "\033[;31m" + str + "\033[0m"; }
 ///> Colorizing a std::string for std::cout. Bold red is for errors.
