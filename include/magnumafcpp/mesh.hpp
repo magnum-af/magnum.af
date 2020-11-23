6a6b8b4e7bf95ef2e30b51daba6c177812319b97
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2020-11-23 23:33:02 +0100

    mesh removing cpp file as shrinked to ctor only

--- include/magnumafcpp/mesh.hpp
+++ include/magnumafcpp/mesh.hpp
@@ -1,19 +1,22 @@
 #pragma once
 #include "arrayfire.h"
-#include <array>
-#include <cstdint>
 #include <iostream>
 
 namespace magnumafcpp {
 
 struct Mesh {
-    Mesh(unsigned nx, unsigned ny, unsigned nz, double dx, double dy, double dz);
+    Mesh(unsigned nx, unsigned ny, unsigned nz, double dx, double dy, double dz)
+        : n0(nx), n1(ny), n2(nz), dx(dx), dy(dy), dz(dz) {}
 
     unsigned n0, n1, n2;             // Number of cells in x, y, z
     double dx, dy, dz;               // Distance between cells
 };
 
-std::ostream& operator<<(std::ostream& os, const Mesh& mesh);
+inline std::ostream& operator<<(std::ostream& os, const Mesh& mesh) {
+    os << "n0=" << mesh.n0 << " n1=" << mesh.n1 << " n2=" << mesh.n2 << " dx=" << mesh.dx << " dy=" << mesh.dy
+       << " dz=" << mesh.dz;
+    return os;
+}
 
 // Dimension for scalar field on mesh, i.e. [nx, ny, nz, 1]
 inline af::dim4 dims_scalar(Mesh mesh) { return af::dim4(mesh.n0, mesh.n1, mesh.n2, 1); }
