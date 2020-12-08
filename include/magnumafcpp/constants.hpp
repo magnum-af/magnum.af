cde2d527a45702d5e664e1b60240a43f9d81b1f7
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2020-12-08 21:58:43 +0100

    adding conversion functions

--- include/magnumafcpp/constants.hpp
+++ include/magnumafcpp/constants.hpp
@@ -13,10 +13,27 @@ constexpr double gamma = 1.760859644e11 * mu0; ///< [m A^-1 s^-1] gyromagnetic r
 
 constexpr double mu_b = 9.274009994e-24; ///< [J/T] Bohr magneton mu_bohr
 
-constexpr double e = -1.6021766208e-19; ///< [C] Elementary charge e (including minus sign)
+constexpr double e_neg = -1.6021766208e-19; ///< [C] Elementary charge e (including minus sign)
+
+constexpr double e_abs = 1.6021766208e-19; ///< [C] Absolute value of the elementary charge e
 
 constexpr double kb = 1.38064852e-23; ///< [J/K] Boltzmann constant kb
 
 constexpr double hbar = 1.0545718e-34; ///< [J s] Reduced Planck constant
 } // namespace constants
+
+namespace conversion {
+/// Conversion of Ampere per meter [A/m] to Tesla [T]
+template <typename T> T Am_to_Tesla(T Ampere_per_meter) { return Ampere_per_meter * constants::mu0; }
+
+/// Conversion of Tesla [T] to Ampere per meter [A/m]
+template <typename T> T Tesla_to_Am(T Tesla) { return Tesla / constants::mu0; }
+
+/// Conversion of Joule [J] to electron volt [eV]
+template <typename T> T J_to_eV(T Joule) { return Joule / constants::e_abs; }
+
+/// Conversion of electron volt [eV] to Joule [J]
+template <typename T> T eV_to_J(T eV) { return eV * constants::e_abs; }
+} // namespace conversion
+
 } // namespace magnumafcpp
--- src/field_terms/micro/spin_transfer_torque_field.cpp
+++ src/field_terms/micro/spin_transfer_torque_field.cpp
@@ -12,7 +12,7 @@ SpinTransferTorqueField::SpinTransferTorqueField(long int polarization_field_ptr
 }
 
 af::array SpinTransferTorqueField::h(const State& state) const {
-    return -j_e * constants::hbar / (2. * constants::e * constants::mu0 * state.Ms) *
+    return -j_e * constants::hbar / (2. * constants::e_neg * constants::mu0 * state.Ms) *
            (nu_dampinglike * cross4(state.m, polarization_field.array) + nu_fieldlike * polarization_field.array);
 }
 } // namespace magnumafcpp
