50cb769426414227d65cc9a94abd003615fdfab3
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2021-02-02 17:22:09 +0100

    update doc for bs23.hpp

--- include/magnumafcpp/integrators/bs23.hpp
+++ include/magnumafcpp/integrators/bs23.hpp
@@ -1,24 +1,30 @@
 #pragma once
 #include <tuple>
-namespace magnumafcpp {
 
-///
-/// /param
+/// Runge-Kutta methods for the integration of ordinary differential equations.
+namespace magnumafcpp::integrator {
+
+/// Bogacki–Shampine 2nd/3rd order method using FSAL property
+/// \param k1 FSAL k4 stage of previous step, used as k1
 template <typename D, typename T, typename F, typename... Args>
 std::tuple<D, T, T, T> BS23(const D t_n, const D dt, const T& y_n, const T& k1, const F& f, const Args&... args) {
     const T k2 = dt * f(t_n + 1. / 2. * dt, y_n + 1. / 2. * k1, args...);
     const T k3 = dt * f(t_n + 3. / 4. * dt, y_n + 3. / 4. * k2, args...);
 
-    const T dy = 2. / 9. * k1 + 1. / 3. * k2 + 4. / 9. * k3;
+    const T dy = 2. / 9. * k1 + 1. / 3. * k2 + 4. / 9. * k3; // 3rd order estimate (diff, w.o. y_n)
     const T k4 = dt * f(t_n + dt, y_n + dy, args...);
-    const T rk_error = dy - (7. / 24. * k1 + 1. / 4. * k2 + 1. / 3. * k3 + 1. / 8. * k4);
+    const T dz = 7. / 24. * k1 + 1. / 4. * k2 + 1. / 3. * k3 + 1. / 8. * k4; // 2nd order estimate (diff, w.o. y_n)
+    const T rk_error = dy - dz;
     return {t_n + dt, y_n + dy, rk_error, k4};
 }
 
+/// Bogacki–Shampine 2nd/3rd order method.
+/// Uses 3rd order (dy) for propagation and 2nd order (rk_error) for error estimate.
+/// returns {t_n + dt, y_n + dy, rk_error, k4}
 template <typename D, typename T, typename F, typename... Args>
 std::tuple<D, T, T, T> BS23(const D t_n, const D dt, const T& y_n, const F& f, const Args&... args) {
     const T k1 = dt * f(t_n, y_n, args...);
-    return {BS23(t_n, dt, y_n, k1, f, args...)};
+    return BS23(t_n, dt, y_n, k1, f, args...);
 }
 
-} // namespace magnumafcpp
+} // namespace magnumafcpp::integrator
--- test/integrators/bs23.cpp
+++ test/integrators/bs23.cpp
@@ -13,18 +13,19 @@ TEST(BS23, integrate_analytical_in_double) {
     double t = 0;
     double y = 1;
     const double dt = 1e-2;
-    const std::size_t imax = 1000;
+    const std::size_t imax = 100000;
 
     // initial step
     double rkerror;
     double k4;
-    std::tie(t, y, rkerror, k4) = BS23(t, dt, y, f);
+    std::tie(t, y, rkerror, k4) = integrator::BS23(t, dt, y, f);
     EXPECT_NEAR(y, analytic_result(t), 4e-6);
 
     // steps using FSAL
     for (std::size_t i = 0; i < imax; i++) {
-        std::tie(t, y, rkerror, k4) = BS23(t, dt, y, k4, f);
-        EXPECT_NEAR(y, analytic_result(t), 4e-6);
+        std::tie(t, y, rkerror, k4) = integrator::BS23(t, dt, y, k4, f);
+        EXPECT_NEAR(y, analytic_result(t), 1e-0 * (i + 1) * std::pow(dt, 3)); // scaling with 3rd order in dt
+        // EXPECT_NEAR(y, analytic_result(t), 4e-6); // for 1000 steps
         // std::cout << std::setprecision(32) << "i=" << i << ", t= " << t << ", y= " << y << '\n';
     }
     // Note: numerically exact same results compared to only using BS23 without k4 argument.
@@ -36,10 +37,27 @@ TEST(BS23, integrate_analytical_in_af_array) {
     af::array y = af::constant(1., 1, f64);
     const double dt = 1e-2;
     const std::size_t imax = 1000;
+
+    af::array rkerror;
+    af::array k4;
+    std::tie(t, y, rkerror, k4) = integrator::BS23(t, dt, y, f);
+    EXPECT_NEAR(y.scalar<double>(), analytic_result(t), 4e-6);
+
     for (std::size_t i = 0; i < imax; i++) {
-        af::array rkerror;
-        af::array k4;
-        std::tie(t, y, rkerror, k4) = BS23(t, dt, y, f);
+        // TODO Performance issue here:
+        // call with k4 overload is far slower as without, but why? Probably memory allocation issue with af::array
+        // (double version above is super fast, 31ms for 1e6 evals)
+        // cpu factor 50, opencl 1.75!
+        // std::tie(t, y, rkerror, k4) = integrator::BS23(t, dt, y, f); // fast (cpu: 0,117s opencl: 0,425s)
+        std::tie(t, y, rkerror, k4) = integrator::BS23(t, dt, y, k4, f); // slow (cpu: 3,393s opencl: 0,742s)
+
+        // // exactly the same numerical results as with k4 BS23:
+        // const auto [t_, y_, rkerror_, k4_] = integrator::BS23(t, dt, y, k4, f);
+        // t = t_;
+        // y = y_;
+        // rkerror = rkerror_;
+        // k4 = k4_;
+
         EXPECT_NEAR(y.scalar<double>(), analytic_result(t), 4e-6);
         // std::cout << std::setprecision(32) << "i=" << i << ", t= " << t << ", y= " << y.scalar<double>() << '\n';
     }
