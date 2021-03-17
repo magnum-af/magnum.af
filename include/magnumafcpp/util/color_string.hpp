ac6148984599cc095375f08c351a4394227b46b1
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2021-03-17 13:44:11 +0100

    magnumafcpp/util/color_string.hpp

--- include/magnumafcpp/util/color_string.hpp
+++ include/magnumafcpp/util/color_string.hpp
@@ -17,9 +17,9 @@ inline std::string bold_green(const std::string& str) { return "\033[1;32m" + st
 inline std::string green(const std::string& str) { return "\033[;32m" + str + "\033[0m"; }
 
 ///> Printing a colorized Info string
-inline const char* info() { return "\33[0;32mInfo:\33[0m "; }
+inline const char* info() { return "\33[0;32mInfo:\33[0m"; }
 
 ///> Printing a colorized Warning string
-inline const char* warning() { return "\33[1;31mWarning:\33[0m "; }
+inline const char* warning() { return "\33[1;31mWarning:\33[0m"; }
 
 } // namespace magnumafcpp::color_string
