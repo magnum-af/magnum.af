77f4706d14cef88c205f483d952e7af30e42d0c9
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2021-04-06 11:31:23 +0200

    math: mean using T not double

--- include/magnumafcpp/math.hpp
+++ include/magnumafcpp/math.hpp
@@ -7,9 +7,9 @@ namespace magnumafcpp::math{
 /// Calculate mean along first three dimensions of af::array.
 template <typename T> std::array<T, 3> mean_3d(const af::array& a) {
     const auto mean = af::mean(af::mean(af::mean(a, 0), 1), 2);
-    const double mx = mean(0, 0, 0, 0).scalar<T>();
-    const double my = mean(0, 0, 0, 1).scalar<T>();
-    const double mz = mean(0, 0, 0, 2).scalar<T>();
+    const T mx = mean(0, 0, 0, 0).scalar<T>();
+    const T my = mean(0, 0, 0, 1).scalar<T>();
+    const T mz = mean(0, 0, 0, 2).scalar<T>();
     return {mx, my, mz};
 }
 
