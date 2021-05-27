ba128f2f2bf588c565eb334d61908b30e003ba4f
Paul Heistracher <paul.thomas.heistracher@univie.ac.at>
2021-05-27 11:55:21 +0200

    update bash/scan_build_clang.sh

--- bash/scan_build_clang.sh
+++ bash/scan_build_clang.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -xe
 # building with clang static analyzer
 
+# call into directory ../ w.r.t. this script
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" && cd ..
+
 mkdir -p build_analyze && cd build_analyze
+
+# Note: $CXX is ignored, scan-build does use clang-compiler anyway
 scan-build cmake ..
 scan-build make -j
